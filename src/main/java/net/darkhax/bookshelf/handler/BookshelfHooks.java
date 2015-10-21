8a621c1069fbdbca72058e95d4c19e20e29a08ea
Darkhax <darklime@live.ca>
2015-10-21 13:31:45 -0600

    Added more documentation.

--- src/main/java/net/darkhax/bookshelf/handler/BookshelfHooks.java
+++ src/main/java/net/darkhax/bookshelf/handler/BookshelfHooks.java
@@ -43,28 +43,61 @@ public class BookshelfHooks {
         return enchantments;
     }
     
+    /**
+     * A hook to trigger the MobSpawnerSpawnEvent. This hook is only triggered by the vanilla
+     * monster spawner, however it is possible for other mods to also trigger this event.
+     * 
+     * @param entity: An instance of the entity that is attempting to spawn.
+     * @return boolean: Whether or not the event should be allowed to happen.
+     */
     public static boolean doEntitySpawnSpawnerCheck (Entity entity) {
         
         MobSpawnerSpawnEvent event = new MobSpawnerSpawnEvent(entity);
         MinecraftForge.EVENT_BUS.post(event);
         Event.Result result = event.getResult();
-        
-        if (result == Event.Result.DEFAULT)
-            return !(entity instanceof EntityLiving) || ((EntityLiving) entity).getCanSpawnHere();
-        else
-            return result == Event.Result.ALLOW;
+        return (result == Event.Result.DEFAULT) ? !(entity instanceof EntityLiving) || ((EntityLiving) entity).getCanSpawnHere() : result == Event.Result.ALLOW;
     }
     
+    /**
+     * A hook to trigger the PistonExtendedEvent. By default, only the piston and sticky piston
+     * will trigger this event, however it is possible for other mods to trigger.
+     * 
+     * @param x: The X coordinate of the piston block.
+     * @param y: The Y coordinate of the piston block.
+     * @param z: The Z coordinate of the piston block.
+     * @param world: An instance of the world in which the piston is being updated.
+     * @param block: An instance of the block acting as the piston.
+     * @param blockMetadata: The meta value for the block acting as the piston.
+     * @param sticky: Whether or not the piston is sticky.
+     * @param facing: The direction that the piston is facing.
+     * @return boolean: Whether or not the piston should be allowed to extend.
+     */
     public static boolean onPistonExtend (int x, int y, int z, World world, Block block, int blockMetadata, int facing, boolean sticky) {
         
         return MinecraftForge.EVENT_BUS.post(new PistonEvent.PistonExtendEvent(x, y, z, world, block, blockMetadata, sticky, EnumFacing.values()[facing]));
     }
     
+    /**
+     * A hook to trigger the PistonRetractEvent. By default only the piston and sticky piston
+     * will trigger this event. It is however, possible for other mods to also trigger this
+     * event.
+     * 
+     * @param x: The X coordinate of the piston block.
+     * @param y: The Y coordinate of the piston block.
+     * @param z: The Z coordinate of the piston block.
+     * @param world: An instance of the world in which the piston is being updated.
+     * @param block: An instance of the block acting as the piston.
+     * @param blockMetadata: The meta value for the block acting as the piston.
+     * @param sticky: Whether or not the piston is sticky.
+     * @param facing: The direction that the piston is facing.
+     * @return boolean: Whether or not the piston should be allowed to retract.
+     */
     public static boolean onPistonRetract (int x, int y, int z, World world, Block block, int blockMetadata, int facing, boolean sticky) {
         
         return MinecraftForge.EVENT_BUS.post(new PistonEvent.PistonRetractEvent(x, y, z, world, block, blockMetadata, sticky, EnumFacing.values()[facing]));
     }
     
+    // TODO fix this event and discuss documentation
     public static boolean canPushBlock (Block pushedBlock, World world, int x, int y, int z) {
         
         Block block = world.getBlock(x, y, z);
