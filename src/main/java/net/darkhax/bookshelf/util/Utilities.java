c732812143dcc656f1bfb3d7318c00b48f1d7326
Darkhax <darklime@live.ca>
2015-10-24 14:04:51 -0600

    Imported model syncing method from Elysian project.

--- src/main/java/net/darkhax/bookshelf/util/Utilities.java
+++ src/main/java/net/darkhax/bookshelf/util/Utilities.java
@@ -18,8 +18,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -700,6 +702,36 @@ public class Utilities {
         
         Color color = new Color(colorVal);
         GL11.glColor3f(((float) color.getRed() / 255f), ((float) color.getGreen() / 255f), ((float) color.getBlue() / 255f));
+    }
+    
+    /**
+     * Synchronizes a ModelBiped to an EntityLivingBase. This method is intended for use in
+     * rendering armor models which use ModelBiped as a base.
+     * 
+     * @param armorModel: The armor model to synchronize to the entity.
+     * @param armorSlot: The type of armor which is being used.
+     * @param entity: The entity to synchronize the armor model to.
+     */
+    @SideOnly(Side.CLIENT)
+    public static void synArmorModelToEntity (ModelBiped armorModel, int armorSlot, EntityLivingBase entity) {
+        
+        if (armorModel == null || entity == null)
+            return;
+            
+        armorModel.bipedHead.showModel = armorSlot == 0;
+        armorModel.bipedHeadwear.showModel = armorSlot == 0;
+        armorModel.bipedBody.showModel = armorSlot == 1 || armorSlot == 2;
+        armorModel.bipedRightArm.showModel = armorSlot == 1;
+        armorModel.bipedLeftArm.showModel = armorSlot == 1;
+        armorModel.bipedRightLeg.showModel = armorSlot == 2 || armorSlot == 3;
+        armorModel.bipedLeftLeg.showModel = armorSlot == 2 || armorSlot == 3;
+        armorModel.isSneak = entity.isSneaking();
+        armorModel.isRiding = entity.isRiding();
+        armorModel.isChild = entity.isChild();
+        armorModel.heldItemRight = entity.getEquipmentInSlot(0) != null ? 1 : 0;
+        
+        if (entity instanceof EntityPlayer)
+            armorModel.aimedBow = ((EntityPlayer) entity).getItemInUseDuration() > 2;
     }
     
     public static class UnsupportedTypeException extends RuntimeException {
