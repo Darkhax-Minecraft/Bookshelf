ce3b9da364b34db670a109557ec4dacb67b6e6bc
Darkhax <darklime@live.ca>
2015-10-24 14:06:20 -0600

    Updated the modeled armor to take stacks into consideration.
    Removed type switches and arguments, because this code will be within
    the armor instance, meaning that this.armorType can be called anywhere
    in our code.

--- src/main/java/net/darkhax/bookshelf/items/ItemModelledArmor.java
+++ src/main/java/net/darkhax/bookshelf/items/ItemModelledArmor.java
@@ -2,86 +2,60 @@ package net.darkhax.bookshelf.items;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.darkhax.bookshelf.util.Utilities;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 
 public abstract class ItemModelledArmor extends ItemArmor {
     
-    public ItemModelledArmor(ArmorMaterial material, int type) {
-        super(material, 4, type);
+    /**
+     * A special implementation of ItemArmor which provides built in support for special armor
+     * models, and their respective textures.
+     * 
+     * @param material: An ArmorMaterial to use for this armor types. Used for durability,
+     *            protection amounts, and so on.
+     * @param armorType: The type of armor. 0 is helmet, 1 is plate, 2 is legs and 3 is boots
+     */
+    public ItemModelledArmor(ArmorMaterial material, int armorType) {
+        
+        super(material, 4, armorType);
     }
     
     @Override
     public String getArmorTexture (ItemStack stack, Entity entity, int slot, String type) {
         
-        ItemArmor item = (ItemArmor) stack.getItem();
-        switch (item.armorType) {
-            case 2: {
-                return getModelTextureName() + "_1.png";
-            }
-            default: {
-                return getModelTextureName() + "_0.png";
-            }
-        }
+        return getModelTextureName(stack);
     }
     
     @Override
     @SideOnly(Side.CLIENT)
-    public ModelBiped getArmorModel (EntityLivingBase entityLiving, ItemStack itemStack, int armorSlot) {
+    public ModelBiped getArmorModel (EntityLivingBase entityLiving, ItemStack stack, int armorSlot) {
         
-        int type = ((ItemArmor) itemStack.getItem()).armorType;
-        ModelBiped armorModel;
-        if (type == 1 || type == 3) {
-            armorModel = getArmorModel(0);
-        }
-        else {
-            armorModel = getArmorModel(1);
-        }
-        
-        if (armorModel != null) {
-            armorModel.bipedHead.showModel = armorSlot == 0;
-            armorModel.bipedHeadwear.showModel = armorSlot == 0;
-            armorModel.bipedBody.showModel = armorSlot == 1 || armorSlot == 2;
-            armorModel.bipedRightArm.showModel = armorSlot == 1;
-            armorModel.bipedLeftArm.showModel = armorSlot == 1;
-            armorModel.bipedRightLeg.showModel = armorSlot == 2 || armorSlot == 3;
-            armorModel.bipedLeftLeg.showModel = armorSlot == 2 || armorSlot == 3;
-            armorModel.isSneak = entityLiving.isSneaking();
-            armorModel.isRiding = entityLiving.isRiding();
-            armorModel.isChild = entityLiving.isChild();
-            armorModel.heldItemRight = entityLiving.getEquipmentInSlot(0) != null ? 1 : 0;
-            
-            if (entityLiving instanceof EntityPlayer) {
-                armorModel.aimedBow = ((EntityPlayer) entityLiving).getItemInUseDuration() > 2;
-            }
-            
-            return armorModel;
-        }
-        
-        return null;
+        ModelBiped armorModel = getArmorModel(stack);
+        Utilities.synArmorModelToEntity(armorModel, armorSlot, entityLiving);
+        return armorModel;
     }
     
     /**
-     * Used to give each armor piece the right model.
-     *
-     * @param pass: The current pass Minecraft is rendering. This is 1 for the leggings, and 0
-     *            for all other pieces.
-     * @return ModelBiped: The model for the armor piece.
+     * Provides the correct model for the piece of armor being rendered.
+     * 
+     * @param stack: The ItemStack of the armor that is being rendered.
+     * @return ModelBiped: The model that should be rendered for the given piece of armor.
      */
     @SideOnly(Side.CLIENT)
-    public abstract ModelBiped getArmorModel (int pass);
+    public abstract ModelBiped getArmorModel (ItemStack stack);
     
     /**
-     * Used to give the model its texture.
-     *
-     * @return String: The full string path to the texture. Minecraft will use the texture file
-     *         ending with "_0" for the leggings, and "_1" for the other pieces. Make sure NOT
-     *         to add the suffices to the texture path. EG:
-     *         testmod:textures/entity/player/armor/player_armor_test
+     * Used to generate the path for the texture that should be used for the model. You do not
+     * need to worry about adding the .png extension, it is added for you.
+     * 
+     * @param stack: The ItemStack of the armor that is being rendered.
+     * @return String: The full string path to the texture. Example:
+     *         yourmod:textures/entity/player/armor/armor_texture
      */
-    public abstract String getModelTextureName ();
+    @SideOnly(Side.CLIENT)
+    public abstract String getModelTextureName (ItemStack stack);
 }
