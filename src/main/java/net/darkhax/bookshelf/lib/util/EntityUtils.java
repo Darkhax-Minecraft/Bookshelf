cb198644fc79f66cd0adfc5b1c24dbc061962938
Darkhax <darklime@live.ca>
2015-11-03 18:39:10 -0700

    Added method to push an entity towards another entity.

--- src/main/java/net/darkhax/bookshelf/lib/util/EntityUtils.java
+++ src/main/java/net/darkhax/bookshelf/lib/util/EntityUtils.java
@@ -20,4 +20,27 @@ public class EntityUtils {
         
         return Math.sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ);
     }
+    
+    /**
+     * Pushes an entity towards another one.
+     * 
+     * @param entityToMove: The entity that should be pushed towards the other entity.
+     * @param destination: The destination entity, that the entity to move should be pushed
+     *            towards.
+     * @param force: The amount of force to push the entityToMove with.
+     */
+    public static void pushTowards (Entity entityToMove, Entity destination, double force) {
+        
+        double distanceX = destination.posX - entityToMove.posX;
+        double distanceY = destination.posY - entityToMove.posY;
+        double distanceZ = destination.posZ - entityToMove.posZ;
+        double distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ);
+        
+        if (distance > 0) {
+            
+            entityToMove.motionX = distanceX / distance * force;
+            entityToMove.motionY = distanceY / distance * force;
+            entityToMove.motionZ = distanceZ / distance * force;
+        }
+    }
 }
