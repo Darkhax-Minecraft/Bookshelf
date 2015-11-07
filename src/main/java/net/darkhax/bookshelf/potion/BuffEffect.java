be5e6bbde3e91ab853f9a5052ffcc14816dc5c08
Darkhax <darklime@live.ca>
2015-11-07 00:39:13 -0700

    Added documentation, cleaned up toString, modified access modifiers.

--- src/main/java/net/darkhax/bookshelf/potion/BuffEffect.java
+++ src/main/java/net/darkhax/bookshelf/potion/BuffEffect.java
@@ -4,10 +4,28 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public class BuffEffect {
     
+    /**
+     * The Buff being applied by this effect.
+     */
     private Buff buff;
-    private int duration;
-    private int power;
     
+    /**
+     * The duration of the effect in ticks. 20 ticks is 1 second.
+     */
+    public int duration;
+    
+    /**
+     * The strength of this effect.
+     */
+    public int power;
+    
+    /**
+     * Constructs a new BuffEffect.
+     * 
+     * @param buff: The buff to create the effect for.
+     * @param duration: The duration of the effect.
+     * @param power: The strength of the effect.
+     */
     public BuffEffect(Buff buff, int duration, int power) {
         
         this.buff = buff;
@@ -15,50 +33,45 @@ public class BuffEffect {
         this.power = power;
     }
     
+    /**
+     * Writes this BuffEffect to an NBTTagCompound so that it can be saved.
+     * 
+     * @param nbt: The NBTTagCompound to write to.
+     */
     public void writeToNBT (NBTTagCompound nbt) {
         
         NBTTagCompound tag = new NBTTagCompound();
-        tag.setString("buff", buff.getPotionName());
-        tag.setInteger("duration", getDuration());
-        tag.setInteger("power", getPower());
+        tag.setString("buff", this.buff.getPotionName());
+        tag.setInteger("duration", this.duration);
+        tag.setInteger("power", this.power);
         nbt.setTag("BookshelfBuff", tag);
     }
     
+    /**
+     * Reads a BuffEffect from an NBTTagCompound.
+     * 
+     * @param nbt: The NBTTagCompound to read from.
+     * @return BuffEffect: The BuffEffect that has been recovered from the NBTTagCompound.
+     */
     public static BuffEffect readFromNBT (NBTTagCompound nbt) {
         
         NBTTagCompound tag = nbt.getCompoundTag("BookshelfBuff");
         return new BuffEffect(BuffHelper.getBuffFromString(tag.getString("buff")), tag.getInteger("duration"), tag.getInteger("power"));
     }
     
+    /**
+     * Provides the buff used in this BuffEffect.
+     * 
+     * @return Buff: The Buff used in this BuffEffect.
+     */
     public Buff getBuff () {
         
         return buff;
     }
     
-    public int getDuration () {
-        
-        return duration;
-    }
-    
-    public int getPower () {
-        
-        return power;
-    }
-    
-    public void setDuration (int duration) {
-        
-        this.duration = duration;
-    }
-    
-    public void setPower (int power) {
-        
-        this.power = power;
-    }
-    
     @Override
     public String toString () {
         
-        return "BuffEffect [buff=" + getBuff() + ", duration=" + getDuration() + ", power=" + getPower() + "]";
+        return buff.getPotionName() + " duration: " + duration + " power: " + power;
     }
-    
 }
