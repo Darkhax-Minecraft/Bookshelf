ead06cc17673f52802fe96e505bff89aa0716c93
Darkhax <darklime@live.ca>
2015-11-07 01:20:27 -0700

    Documentation

--- src/main/java/net/darkhax/bookshelf/common/EntityProperties.java
+++ src/main/java/net/darkhax/bookshelf/common/EntityProperties.java
@@ -40,35 +40,65 @@ public class EntityProperties implements IExtendedEntityProperties {
     
     }
     
+    /**
+     * Synchronizes the EntityProperties data.
+     */
     public void sync () {
         
         Bookshelf.network.sendToAll(new PacketSyncPlayerProperties(this));
     }
     
+    /**
+     * Retrieves the EntityProperties instance from an Entity.
+     * 
+     * @param entity: The Entity to retrieve from.
+     * @return EntityProperties: The EntityProperties instance for that entity.
+     */
     public static EntityProperties getProperties (EntityLivingBase entity) {
         
         return (EntityProperties) entity.getExtendedProperties(PROP_NAME);
     }
     
+    /**
+     * Sets the EntityProperties instance for an entity.
+     * 
+     * @param entity: The entity to set to.
+     * @return EntityProperties: The new EntityProperties instance.
+     */
     public static EntityProperties setProperties (EntityLivingBase entity) {
         
         entity.registerExtendedProperties(PROP_NAME, new EntityProperties(entity));
         return getProperties(entity);
     }
     
+    /**
+     * A check to determine whether or not an Entity has an instance of EntityProperties.
+     * 
+     * @param entity: The Entity to check.
+     * @return boolean: Whether or not the entity has an instance of EntityProperties.
+     */
     public static boolean hasProperties (EntityLivingBase entity) {
         
         return getProperties(entity) != null;
     }
     
+    /**
+     * Retrieves a list of buffs from the EntityProperties instance.
+     * 
+     * @return NBTTagList: A list of buffs in NBT.
+     */
     public NBTTagList getBuffs () {
         
         return buffs;
     }
     
+    /**
+     * Sets a list of Buffs to this EntityProperties instance.
+     * 
+     * @param buffs: The list of buffs to set.
+     */
     public void setBuffs (NBTTagList buffs) {
         
         this.buffs = buffs;
     }
-    
 }
