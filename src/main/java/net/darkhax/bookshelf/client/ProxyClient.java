9270a0238a5318aecc22155de0fab531060ef58d
Darkhax <darklime@live.ca>
2015-11-15 11:05:51 -0700

    Made the gui fields client sided only.

--- src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
+++ src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
@@ -1,5 +1,6 @@
 package net.darkhax.bookshelf.client;
 
+import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
 
 import net.minecraftforge.common.MinecraftForge;
@@ -9,6 +10,7 @@ import cpw.mods.fml.relauncher.ReflectionHelper;
 
 import net.darkhax.bookshelf.common.ProxyCommon;
 import net.darkhax.bookshelf.lib.util.PlayerUtils;
+import net.darkhax.bookshelf.lib.util.RenderUtils;
 
 public class ProxyClient extends ProxyCommon {
     
@@ -16,6 +18,8 @@ public class ProxyClient extends ProxyCommon {
     public void preInit () {
         
         MinecraftForge.EVENT_BUS.register(new RenderingHandler());
+        RenderUtils.guiLeft = ReflectionHelper.findField(GuiContainer.class, "i", "field_147003_i", "guiLeft");
+        RenderUtils.guiTop = ReflectionHelper.findField(GuiContainer.class, "r", "field_147009_r", "guiTop");
         PlayerUtils.currentBlockDamage = ReflectionHelper.findField(PlayerControllerMP.class, "g", "field_78770_f", "curBlockDamageMP");
         FMLInterModComms.sendMessage("llibrary", "update-checker", "https://raw.githubusercontent.com/Darkhax-Minecraft/Bookshelf/master/versions.json");
     }
