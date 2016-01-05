2f7eaa06a4c58c02f87c28a9a259550d4d35af4f
Darkhax <darklime@live.ca>
2016-01-04 20:56:52 -0700

    Re-added private fields.

--- src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
+++ src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
@@ -1,12 +1,18 @@
 package net.darkhax.bookshelf.client;
 
 import net.darkhax.bookshelf.common.ProxyCommon;
+import net.darkhax.bookshelf.lib.util.PlayerUtils;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class ProxyClient extends ProxyCommon {
     
     @Override
     public void preInit () {
     
+        // RenderUtils.guiLeft = ReflectionHelper.findField(GuiContainer.class, "i", "field_147003_i", "guiLeft");
+        // RenderUtils.guiTop = ReflectionHelper.findField(GuiContainer.class, "r", "field_147009_r", "guiTop");
+        PlayerUtils.currentBlockDamage = ReflectionHelper.findField(PlayerControllerMP.class, "g", "field_78770_f", "curBlockDamageMP");
     }
     
     @Override
