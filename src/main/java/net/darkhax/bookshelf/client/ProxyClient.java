194367c2a5dfb290fe6896a2a420c37d9fe0ce1a
Darkhax <darklime@live.ca>
2016-01-05 20:11:11 -0700

    Remoeved reflection code.

--- src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
+++ src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
@@ -1,20 +1,14 @@
 package net.darkhax.bookshelf.client;
 
 import net.darkhax.bookshelf.common.ProxyCommon;
-import net.darkhax.bookshelf.lib.util.PlayerUtils;
-import net.minecraft.client.multiplayer.PlayerControllerMP;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ProxyClient extends ProxyCommon {
     
     @Override
     public void preInit () {
         
-        // RenderUtils.guiLeft = ReflectionHelper.findField(GuiContainer.class, "i",
-        // "field_147003_i", "guiLeft");
-        // RenderUtils.guiTop = ReflectionHelper.findField(GuiContainer.class, "r",
-        // "field_147009_r", "guiTop");
-        PlayerUtils.currentBlockDamage = ReflectionHelper.findField(PlayerControllerMP.class, "g", "field_78770_f", "curBlockDamageMP");
+        MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
     }
     
     @Override
--- src/main/java/net/darkhax/bookshelf/lib/util/PlayerUtils.java
+++ src/main/java/net/darkhax/bookshelf/lib/util/PlayerUtils.java
@@ -1,44 +1,16 @@
 package net.darkhax.bookshelf.lib.util;
 
-import java.lang.reflect.Field;
 import java.util.UUID;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.inventory.Container;
-import net.minecraft.inventory.ContainerPlayer;
-import net.minecraft.inventory.ContainerWorkbench;
-import net.minecraft.inventory.InventoryCrafting;
-import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public final class PlayerUtils {
-    
-    /**
-     * Access to the eventHandler field in an InventoryCrafting instance.
-     */
-    private static final Field eventHandler = ReflectionHelper.findField(InventoryCrafting.class, "eventHandler", "field_70465_c");
-    
-    /**
-     * Access to the thePlayer field in a ContainerPlayer instance.
-     */
-    private static final Field containerPlayer = ReflectionHelper.findField(ContainerPlayer.class, "thePlayer", "field_82862_h");
-    
-    /**
-     * Access to the thePlayer field in a SlotCrafting instance.
-     */
-    private static final Field slotPlayer = ReflectionHelper.findField(SlotCrafting.class, "thePlayer", "field_75238_b");
-    
-    /**
-     * Access to the curBlockDamageMP field in the client-side PlayerControllerMP instance.
-     */
-    @SideOnly(Side.CLIENT)
-    public static Field currentBlockDamage;
     
     /**
      * Checks if a specific player can sleep. For this to be true, a player must not already be
@@ -94,37 +66,6 @@ public final class PlayerUtils {
     }
     
     /**
-     * Retrieves the player that is crafting in an InventoryCrafting. This is done through
-     * reflection. The first attempt will try to get the player from a ContainerPlayer, and the
-     * second attempt tries a ContainerWorkbench. If no player is found, null will be thrown.
-     * 
-     * @param inventory: An instance of the InventoryCrafting being used.
-     * @return EntityPlayer: The EntityPlayer that is using the InventoryCrafting. If none is
-     *         found, null will be returned.
-     */
-    public static EntityPlayer getPlayerFromCrafting (InventoryCrafting inventory) {
-        
-        try {
-            
-            Container container = (Container) eventHandler.get(inventory);
-            
-            if (container instanceof ContainerPlayer)
-                return (EntityPlayer) containerPlayer.get(container);
-                
-            else if (container instanceof ContainerWorkbench)
-                return (EntityPlayer) slotPlayer.get(container.getSlot(0));
-                
-        }
-        
-        catch (Exception e) {
-            
-            e.printStackTrace();
-        }
-        
-        return null;
-    }
-    
-    /**
      * Retrieves an instance of the player from the client side. This code only exists in
      * client side code and can not be used in server side code.
      */
@@ -132,36 +73,5 @@ public final class PlayerUtils {
     public static EntityPlayer getClientPlayer () {
         
         return Minecraft.getMinecraft().thePlayer;
-    }
-    
-    /**
-     * A client sided method used to retrieve the progression of the block currently being
-     * mined by the player. This method is client side only, and refers to only the one
-     * instance of the player. Do not try to use this method to get data for multiple players,
-     * or for server sided things.
-     * 
-     * @return float: A float value representing how much time is left for the block being
-     *         broken to break. 0 = no damage has been done. 1 = the block is broken.
-     */
-    @SideOnly(Side.CLIENT)
-    public static float getBlockDamage () {
-        
-        if (currentBlockDamage == null)
-            return 0;
-            
-        try {
-            
-            return currentBlockDamage.getFloat(Minecraft.getMinecraft().playerController);
-        }
-        
-        catch (IllegalArgumentException e) {
-        
-        }
-        
-        catch (IllegalAccessException e) {
-        
-        }
-        
-        return 0;
     }
 }
