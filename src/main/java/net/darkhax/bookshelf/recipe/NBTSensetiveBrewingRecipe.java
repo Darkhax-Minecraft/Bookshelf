1360d7e4464fcc2b212a748f84b1554b419323c3
Darkhax <darklime@live.ca>
2016-01-26 14:54:13 -0700

    Added a new NBT sensitive brewing recipe.

---
+++ src/main/java/net/darkhax/bookshelf/recipe/NBTSensetiveBrewingRecipe.java
@@ -0,0 +1,27 @@
+package net.darkhax.bookshelf.recipe;
+
+import net.darkhax.bookshelf.lib.util.ItemStackUtils;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.brewing.AbstractBrewingRecipe;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class NBTSensetiveBrewingRecipe extends AbstractBrewingRecipe<ItemStack> {
+    
+    public NBTSensetiveBrewingRecipe(ItemStack input, ItemStack ingredient, ItemStack output) {
+        
+        super(input, ingredient, output);
+    }
+    
+    @Override
+    public boolean isIngredient (ItemStack stack) {
+        
+        boolean isIngredient = OreDictionary.itemMatches(this.ingredient, stack, false) && ItemStackUtils.areStacksEqual(this.ingredient, stack, true);
+        return isIngredient;
+    }
+    
+    @Override
+    public boolean isInput(ItemStack stack) {
+        
+        return OreDictionary.itemMatches(this.input, stack, false) && ItemStackUtils.areStacksEqual(this.input, stack, true);
+    }
+}
