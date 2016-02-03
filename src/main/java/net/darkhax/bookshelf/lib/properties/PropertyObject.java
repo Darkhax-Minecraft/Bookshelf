07312a19466e907cfb148e59b1ffdcea4b185cf9
Darkhax <darklime@live.ca>
2016-02-03 14:24:24 -0700

    Created a super basic Object property for block states.

---
+++ src/main/java/net/darkhax/bookshelf/lib/properties/PropertyObject.java
@@ -0,0 +1,58 @@
+package net.darkhax.bookshelf.lib.properties;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import net.minecraftforge.common.property.IUnlistedProperty;
+
+public class PropertyObject<T> implements IUnlistedProperty<T> {
+    
+    /**
+     * The name of the property.
+     */
+    private final String name;
+    
+    /**
+     * The type of class held by it.
+     */
+    private final Class<T> type;
+    
+    /**
+     * A generic Unlisted Property which can be used to theoretically hold any Object. This
+     * property does not make an attempt to determine whether or not the generic object stored
+     * is valid. The value stored by the property is equal to the object's toString value. For
+     * more complex properties, use a specific property type, or make your own.
+     * 
+     * @param name: The name to use for this property.
+     * @param type: The class type of this property.
+     */
+    public PropertyObject(String name, Class<T> type) {
+        
+        this.name = name;
+        this.type = type;
+    }
+    
+    @Override
+    public String getName () {
+        
+        return name;
+    }
+       
+    @Override
+    public Class<T> getType () {
+        
+        return type;
+    } 
+    
+    @Override
+    public boolean isValid (T object) {
+        
+        return true;
+    }
+
+    @Override
+    public String valueToString (T object) {
+        
+        return object.toString();
+    }
+}
