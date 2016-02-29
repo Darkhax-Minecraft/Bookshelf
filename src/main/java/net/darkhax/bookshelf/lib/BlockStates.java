24b4f5b9424e48c9537c2c3ae1d53b74730f9ed5
Darkhax <darklime@live.ca>
2016-02-29 09:00:34 -0700

    Added new blockstates. #Release

--- src/main/java/net/darkhax/bookshelf/lib/BlockStates.java
+++ src/main/java/net/darkhax/bookshelf/lib/BlockStates.java
@@ -5,7 +5,9 @@ import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.EnumDyeColor;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockStates {
     
@@ -18,6 +20,16 @@ public class BlockStates {
      * Used to hold another block state. This is useful for mimicking other blocks.
      */
     public static final PropertyObject<IBlockState> HELD_STATE = new PropertyObject<IBlockState>("held_state", IBlockState.class);
+    
+    /**
+     * Used to hold an IBlockAccess of the block.
+     */
+    public static final PropertyObject<IBlockAccess> BLOCK_ACCESS = new PropertyObject<>("world", IBlockAccess.class);
+
+    /**
+     * Used to hold the BlockPos of the block.
+     */
+    public static final PropertyObject<BlockPos> BLOCKPOS = new PropertyObject<>("pos", BlockPos.class);
     
     /**
      * Used to determine the color of a block. Only supports the 16 vanilla colors.
