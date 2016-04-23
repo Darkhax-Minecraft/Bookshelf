763572bb4c2f83cbdef7c0c9d58d195808d3aeac
Tyler Hancock <darklime@live.ca>
2016-04-23 11:32:52 -0600

    Added a vanilla armor slot.

---
+++ src/main/java/net/darkhax/bookshelf/inventory/SlotArmor.java
@@ -0,0 +1,60 @@
+package net.darkhax.bookshelf.inventory;
+
+import net.darkhax.bookshelf.lib.util.ItemStackUtils;
+import net.minecraft.entity.Entity;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class SlotArmor extends Slot {
+    
+    /**
+     * The entity to show armor for.
+     */
+    private final Entity entity;
+    
+    /**
+     * The type of equipment held by the slot.
+     */
+    private final EntityEquipmentSlot slotType;
+    
+    /**
+     * Creates a new armor inventory slot. Typically used for player armor in a GUI.
+     * 
+     * @param entity The entity to show armor for.
+     * @param type The type of equipment held by the slot.
+     * @param inventory The inventory of the slot.
+     * @param index The index of the slot.
+     * @param xPosition The x position of the slot.
+     * @param yPosition The y position of the slot.
+     */
+    public SlotArmor(Entity entity, EntityEquipmentSlot type, IInventory inventory, int index, int xPosition, int yPosition) {
+        
+        super(inventory, index, xPosition, yPosition);
+        this.entity = entity;
+        this.slotType = type;
+    }
+    
+    @Override
+    public int getSlotStackLimit () {
+        
+        return 1;
+    }
+    
+    @Override
+    public boolean isItemValid (ItemStack stack) {
+        
+        return (ItemStackUtils.isValidStack(stack) ? stack.getItem().isValidArmor(stack, slotType, entity) : false);
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public String getSlotTexture () {
+        
+        return ItemArmor.EMPTY_SLOT_NAMES[slotType.getIndex()];
+    }
+}
