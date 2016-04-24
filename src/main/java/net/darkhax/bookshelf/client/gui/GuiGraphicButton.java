955e5f776390da09ac40f769ee0257cf142b5019
Tyler Hancock <darklime@live.ca>
2016-04-24 03:29:00 -0600

    Readded the graphic gui button.

---
+++ src/main/java/net/darkhax/bookshelf/client/gui/GuiGraphicButton.java
@@ -0,0 +1,25 @@
+package net.darkhax.bookshelf.client.gui;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+public class GuiGraphicButton extends GuiButton {
+    
+    private ResourceLocation buttonImage = new ResourceLocation("textures/gui/widgets.png");
+    
+    public GuiGraphicButton(int buttonID, int xPosition, int yPosition, ResourceLocation texture) {
+        
+        super(buttonID, xPosition, yPosition, 20, 20, "");
+        this.buttonImage = texture;
+    }
+    
+    @Override
+    public void drawButton (Minecraft mc, int posX, int posY) {
+        
+        super.drawButton(mc, posX, posY);
+        mc.getTextureManager().bindTexture(buttonImage);
+        drawModalRectWithCustomSizedTexture(this.xPosition, this.yPosition, 0f, 0f, 20, 20, 20f, 20f);
+    }
+}
