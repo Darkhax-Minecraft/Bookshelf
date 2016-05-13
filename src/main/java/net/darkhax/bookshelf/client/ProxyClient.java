e4f0a30be2a3c7178acd1350202e2514d5183cd4
Tyler Hancock <darklime@live.ca>
2016-05-13 12:49:57 -0600

    Added cape code and elytra code.

--- src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
+++ src/main/java/net/darkhax/bookshelf/client/ProxyClient.java
@@ -1,13 +1,36 @@
 package net.darkhax.bookshelf.client;
 
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
+
 import net.darkhax.bookshelf.Bookshelf;
 import net.darkhax.bookshelf.block.BlockWoodenShelf;
 import net.darkhax.bookshelf.common.ProxyCommon;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ProxyClient extends ProxyCommon {
+    
+    private static final ExecutorService THREAD_POOL = new ThreadPoolExecutor(0, 2, 1L, TimeUnit.MINUTES, new LinkedBlockingQueue());
+    
+    private static final ResourceLocation CAPE_DARKHAX = new ResourceLocation("bookshelf", "textures/entity/player/cape_darkhax.png");
+    private static final ResourceLocation ELYTRA_DARKHAX = new ResourceLocation("bookshelf", "textures/entity/player/elytra_darkhax.png");
+    private static final ResourceLocation CAPE_SETH = new ResourceLocation("bookshelf", "textures/entity/player/cape_seth.png");
+    private static final ResourceLocation ELYTRA_SETH = new ResourceLocation("bookshelf", "textures/entity/player/elytra_seth.png");
+    private static final ResourceLocation CAPE_SYCO = new ResourceLocation("bookshelf", "textures/entity/player/cape_syco.png");
+    private static final ResourceLocation ELYTRA_SYCO = new ResourceLocation("bookshelf", "textures/entity/player/elytra_syco.png");
     
     @Override
     public void preInit () {
@@ -16,6 +39,8 @@ public class ProxyClient extends ProxyCommon {
         
         for (int meta = 0; meta < 5; meta++)
             ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation("bookshelf:bookshelf_" + BlockWoodenShelf.types[meta], "inventory"));
+            
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
     @Override
@@ -26,5 +51,71 @@ public class ProxyClient extends ProxyCommon {
     @Override
     public void postInit () {
     
+    }
+    
+    @SubscribeEvent
+    public void entityJoinWorld (EntityJoinWorldEvent event) {
+        
+        if (event.getEntity() instanceof AbstractClientPlayer) {
+            
+            final AbstractClientPlayer player = (AbstractClientPlayer) event.getEntity();
+            System.out.println(player.getUniqueID().toString());
+            final UUID id = player.getUniqueID();
+            
+            // Darkhax
+            if (id.toString().equals("d183e5a2-a087-462a-963e-c3d7295f9ec5")) {
+                
+                System.out.println("Darkhax is True");
+                makePlayerFancy(player, CAPE_DARKHAX, ELYTRA_DARKHAX);
+            }
+            
+            // Syco
+            if (id.toString().equals("eec70f59-1543-4580-b4d8-4954a108866b"))
+                makePlayerFancy(player, CAPE_SYCO, ELYTRA_SYCO);
+                
+            // Seth
+            else if (id.toString().equals("90b9f095-ba47-450c-a290-200a5cefe5be"))
+                makePlayerFancy(player, CAPE_SETH, ELYTRA_SETH);
+        }
+    }
+    
+    /**
+     * Attempts to make the player super fancy. Will try to apply the cape texture and the
+     * elytra texture that is passed.
+     * 
+     * @param player The player to make fancy.
+     * @param cape The cape texture to set.
+     * @param elytra The elytra texture to set.
+     */
+    private static void makePlayerFancy (AbstractClientPlayer player, ResourceLocation cape, ResourceLocation elytra) {
+        
+        THREAD_POOL.submit(new Runnable() {
+            
+            @Override
+            public void run () {
+                
+                try {
+                    
+                    Thread.sleep(100);
+                }
+                catch (final InterruptedException e) {
+                    
+                    return;
+                }
+                
+                Minecraft.getMinecraft().addScheduledTask(new Runnable() {
+                    
+                    @Override
+                    public void run () {
+                        
+                        if (cape != null)
+                            RenderUtils.setPlayerTexture(Type.CAPE, player, cape);
+                            
+                        if (elytra != null)
+                            RenderUtils.setPlayerTexture(Type.ELYTRA, player, elytra);
+                    }
+                });
+            }
+        });
     }
 }
