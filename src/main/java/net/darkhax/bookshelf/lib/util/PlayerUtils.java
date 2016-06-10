965ca6d153a176b8629361c149fa799adbcffb2d
Tyler Hancock <darklime@live.ca>
2016-06-10 00:22:12 -0600

    Added a way to get a username from a UUID.

--- src/main/java/net/darkhax/bookshelf/lib/util/PlayerUtils.java
+++ src/main/java/net/darkhax/bookshelf/lib/util/PlayerUtils.java
@@ -1,7 +1,14 @@
 package net.darkhax.bookshelf.lib.util;
 
+import java.io.BufferedReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
+import com.google.common.io.Resources;
+import com.google.gson.stream.JsonReader;
+
+import net.darkhax.bookshelf.lib.Constants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiNewChat;
 import net.minecraft.entity.player.EntityPlayer;
@@ -59,6 +66,44 @@ public final class PlayerUtils {
             }
             
         return null;
+    }
+    
+    /**
+     * Attempts to get the username associated with a UUID from Mojang. If no username is
+     * detected or an exception takes place, the exception message will be returned.
+     * 
+     * @param id The UUID to search for.
+     * @return The name of the player associated to that uuid.
+     */
+    public static String getPlayerNameFromUUID (UUID uuid) {
+        
+        String name = null;
+        
+        try {
+            
+            final BufferedReader reader = Resources.asCharSource(new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.toString().replace("-", "")), StandardCharsets.UTF_8).openBufferedStream();
+            final JsonReader json = new JsonReader(reader);
+            json.beginObject();
+            
+            while (json.hasNext())
+                if (json.nextName().equals("name"))
+                    name = json.nextString();
+                    
+                else
+                    json.skipValue();
+                    
+            json.endObject();
+            json.close();
+            reader.close();
+        }
+        
+        catch (final Exception exception) {
+            
+            Constants.LOG.warn("Could not get name for " + uuid + " " + exception.getMessage());
+            name = exception.getMessage();
+        }
+        
+        return name;
     }
     
     /**
