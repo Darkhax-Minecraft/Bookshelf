0b74f5b9a18763fff4107fc909f7609f061de0ff
Tyler Hancock <darklime@live.ca>
2016-06-10 02:29:17 -0600

    Added supporter heads.

--- src/main/java/net/darkhax/bookshelf/creativetab/CreativeTabSkulls.java
+++ src/main/java/net/darkhax/bookshelf/creativetab/CreativeTabSkulls.java
@@ -3,6 +3,10 @@ package net.darkhax.bookshelf.creativetab;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.darkhax.bookshelf.handler.SupporterHandler;
+import net.darkhax.bookshelf.handler.SupporterHandler.SupporterData;
+import net.darkhax.bookshelf.lib.util.ItemStackUtils;
+import net.darkhax.bookshelf.lib.util.PlayerUtils;
 import net.darkhax.bookshelf.lib.util.SkullUtils;
 import net.darkhax.bookshelf.lib.util.SkullUtils.MHFAccount;
 import net.minecraft.creativetab.CreativeTabs;
@@ -13,14 +17,27 @@ import net.minecraft.item.ItemStack;
 public class CreativeTabSkulls extends CreativeTabs {
     
     private static ItemStack displayStack = null;
+    
+    /**
+     * A cache of all the player skull ItemStacks so they don't need to be recreated.
+     */
     private static List<ItemStack> cache = new ArrayList<ItemStack>();
     
     public CreativeTabSkulls() {
         
         super("bookshelfheads");
+        
         this.setBackgroundImageName("item_search.png");
         displayStack = SkullUtils.createSkull("Darkhax");
         
+        for (final SupporterData data : SupporterHandler.getSupporters())
+            if (data.wantsHead()) {
+                
+                final ItemStack stack = SkullUtils.createSkull(PlayerUtils.getPlayerNameFromUUID(data.getPlayerID()));
+                ItemStackUtils.setLore(stack, new String[] { data.getFormat() + data.getType() });
+                cache.add(stack);
+            }
+            
         for (final MHFAccount mhf : MHFAccount.values())
             cache.add(SkullUtils.createSkull(mhf));
     }
