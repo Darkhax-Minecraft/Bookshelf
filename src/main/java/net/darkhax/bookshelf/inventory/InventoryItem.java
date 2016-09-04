0666edd55f5dcc265ef0f406d2944887336e08bc
Tyler Hancock <darklime@live.ca>
2016-09-04 17:50:47 -0600

    New helper method to get stacks.

--- src/main/java/net/darkhax/bookshelf/inventory/InventoryItem.java
+++ src/main/java/net/darkhax/bookshelf/inventory/InventoryItem.java
@@ -1,5 +1,8 @@
 package net.darkhax.bookshelf.inventory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.darkhax.bookshelf.lib.util.ItemStackUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
@@ -12,7 +15,7 @@ import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.common.util.Constants;
 
 public class InventoryItem extends Item implements IInventory {
-
+    
     /**
      * The item holding the inventory.
      */
@@ -34,10 +37,12 @@ public class InventoryItem extends Item implements IInventory {
     private final String name;
     
     /**
-     * Constructs a new Item Inventory. This is an inventory which is stored on an Item. Such an inventory can not contain items of the same type.
+     * Constructs a new Item Inventory. This is an inventory which is stored on an Item. Such
+     * an inventory can not contain items of the same type.
+     * 
      * @param invItem The item holding the inventory.
      * @param size The size of the inventory.
-     * @param name The name of the inventory. 
+     * @param name The name of the inventory.
      */
     public InventoryItem(ItemStack invItem, int size, String name) {
         
@@ -47,40 +52,40 @@ public class InventoryItem extends Item implements IInventory {
         this.name = name;
         
         ItemStackUtils.prepareDataTag(invItem);
-        readFromNBT();
+        this.readFromNBT();
     }
     
     /**
      * Reads the inventory data from the ItemInventory tag on {@link #invItem}.
      */
-    public void readFromNBT() {
+    public void readFromNBT () {
         
-        NBTTagList items = this.invItem.getTagCompound().getTagList("ItemInventory", Constants.NBT.TAG_COMPOUND);
+        final NBTTagList items = this.invItem.getTagCompound().getTagList("ItemInventory", Constants.NBT.TAG_COMPOUND);
         
         for (int index = 0; index < items.tagCount(); index++) {
             
-            NBTTagCompound itemData = items.getCompoundTagAt(index);
-            int slot = itemData.getInteger("Slot");
+            final NBTTagCompound itemData = items.getCompoundTagAt(index);
+            final int slot = itemData.getInteger("Slot");
             
-            if (slot >= 0 && slot < getSizeInventory())
-                inventory[slot] = ItemStack.loadItemStackFromNBT(itemData);
+            if (slot >= 0 && slot < this.getSizeInventory())
+                this.inventory[slot] = ItemStack.loadItemStackFromNBT(itemData);
         }
     }
     
     /**
      * Writes the inventory data to the ItemInventory tag on {@link #invItem}.
      */
-    public void writeToNBT() {
+    public void writeToNBT () {
         
-        NBTTagList items = new NBTTagList();
+        final NBTTagList items = new NBTTagList();
         
-        for (int index = 0; index < getSizeInventory(); index++) {
+        for (int index = 0; index < this.getSizeInventory(); index++) {
             
-            ItemStack stack = getStackInSlot(index);
+            final ItemStack stack = this.getStackInSlot(index);
             
             if (ItemStackUtils.isValidStack(stack)) {
                 
-                NBTTagCompound itemData = new NBTTagCompound();
+                final NBTTagCompound itemData = new NBTTagCompound();
                 itemData.setInteger("Slot", index);
                 stack.writeToNBT(itemData);
                 items.appendTag(itemData);
@@ -90,132 +95,150 @@ public class InventoryItem extends Item implements IInventory {
         this.invItem.getTagCompound().setTag("ItemInventory", items);
     }
     
+    /**
+     * Gets a List of all the ItemStack inside the inventory from a stack. Allows for access
+     * without creating an entire inventory. Should only be used for reading, and never
+     * writing.
+     * 
+     * @param stack The stack to read from.
+     * @return A List of the stored ItemStack.
+     */
+    public static List<ItemStack> getContents (ItemStack stack) {
+        
+        ItemStackUtils.prepareDataTag(stack);
+        final List<ItemStack> list = new ArrayList<ItemStack>();
+        final NBTTagList items = stack.getTagCompound().getTagList("ItemInventory", Constants.NBT.TAG_COMPOUND);
+        
+        for (int index = 0; index < items.tagCount(); index++)
+            list.add(ItemStack.loadItemStackFromNBT(items.getCompoundTagAt(index)));
+            
+        return list;
+    }
+    
     @Override
     public String getName () {
         
-        return name;
+        return this.name;
     }
-
+    
     @Override
     public boolean hasCustomName () {
         
-        return name.length() > 0;
+        return this.name.length() > 0;
     }
-
+    
     @Override
     public ITextComponent getDisplayName () {
         
         return new TextComponentTranslation(this.name);
     }
-
+    
     @Override
     public int getSizeInventory () {
         
-        return size;
+        return this.size;
     }
-
+    
     @Override
     public ItemStack getStackInSlot (int index) {
         
-        return inventory[index];
+        return this.inventory[index];
     }
-
+    
     @Override
     public ItemStack decrStackSize (int index, int count) {
         
-        ItemStack stack = getStackInSlot(index);
+        ItemStack stack = this.getStackInSlot(index);
         
-        if (ItemStackUtils.isValidStack(stack)) {
-            
+        if (ItemStackUtils.isValidStack(stack))
             if (stack.stackSize > count) {
                 
                 stack = stack.splitStack(count);
-                markDirty();
+                this.markDirty();
             }
             
             else
-                setInventorySlotContents(index, null);
-        }
-        
+                this.setInventorySlotContents(index, null);
+                
         return stack;
     }
-
+    
     @Override
     public ItemStack removeStackFromSlot (int index) {
         
-        ItemStack stack = getStackInSlot(index);
-        setInventorySlotContents(index, null);
+        final ItemStack stack = this.getStackInSlot(index);
+        this.setInventorySlotContents(index, null);
         return stack;
     }
-
+    
     @Override
     public void setInventorySlotContents (int index, ItemStack stack) {
         
-        inventory[index] = stack;
+        this.inventory[index] = stack;
         
-        if (stack != null && stack.stackSize > getInventoryStackLimit())
-            stack.stackSize = getInventoryStackLimit();
-        
-        markDirty();
+        if (stack != null && stack.stackSize > this.getInventoryStackLimit())
+            stack.stackSize = this.getInventoryStackLimit();
+            
+        this.markDirty();
     }
-
+    
     @Override
     public int getInventoryStackLimit () {
         
         return 64;
     }
-
+    
     @Override
     public void markDirty () {
         
-        for (int index = 0; index < getSizeInventory(); index++)
-            if (ItemStackUtils.isValidStack(getStackInSlot(index)) && getStackInSlot(index).stackSize == 0)
-                inventory[index] = null;
-        
-        writeToNBT();
+        for (int index = 0; index < this.getSizeInventory(); index++)
+            if (ItemStackUtils.isValidStack(this.getStackInSlot(index)) && this.getStackInSlot(index).stackSize == 0)
+                this.inventory[index] = null;
+                
+        this.writeToNBT();
     }
-
+    
     @Override
     public boolean isUseableByPlayer (EntityPlayer player) {
         
         return true;
     }
-
+    
     @Override
     public void openInventory (EntityPlayer player) {
-
+    
     }
-
+    
     @Override
     public void closeInventory (EntityPlayer player) {
-        
+    
     }
-
+    
     @Override
     public boolean isItemValidForSlot (int index, ItemStack stack) {
         
         return stack.getItem() != this.invItem.getItem();
     }
-
+    
     @Override
     public int getField (int id) {
         
         return 0;
     }
-
+    
     @Override
     public void setField (int id, int value) {
-        
+    
     }
-
+    
     @Override
     public int getFieldCount () {
         
         return 0;
     }
-
+    
     @Override
     public void clear () {
-        
+    
     }
 }
