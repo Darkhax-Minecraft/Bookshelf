0e281dd3fddacf7915ce9fd6e4cb227cc1dace95
Tyler Hancock <darklime@live.ca>
2016-11-03 16:30:57 -0600

    Updated code for latest Baubles

--- src/main/java/net/darkhax/bookshelf/lib/modutils/baubles/BaublesUtils.java
+++ src/main/java/net/darkhax/bookshelf/lib/modutils/baubles/BaublesUtils.java
@@ -1,145 +1,87 @@
 package net.darkhax.bookshelf.lib.modutils.baubles;
 
+import baubles.api.BaubleType;
 import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
 import net.darkhax.bookshelf.lib.util.ItemStackUtils;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Optional;
 
 public class BaublesUtils {
     
+    public static final int AMULTER = 0;
+    public static final int RING_1 = 1;
+    public static final int RING_2 = 2;
+    public static final int BELT = 3;
+    public static final int HEAD = 4;
+    public static final int BODY = 5;
+    public static final int CHARM = 6;
+    
     /**
-     * Checks if a player has a bauble in the amulet slot.
+     * Attempts to get a bauble from the player.
      * 
-     * @param player The player to check for.
-     * @param item The item you are looking for. Only item, meta are accounted for. See
-     *        {@link ItemStackUtils#areStacksSimilar(ItemStack, ItemStack)} for more info.
-     * @return Whether or not the player has a bauble in the desired slot.
+     * @param player The player to get the bauble from.
+     * @param type The type of bauble being searched for.
+     * @return The first stack found.
      */
     @Optional.Method(modid = "Baubles")
-    public static boolean isPlayerWearingAmulet (EntityPlayer player, ItemStack item) {
+    public static ItemStack getBauble (EntityPlayer player, BaubleType type) {
         
-        return isPlayerWearingBauble(player, item, 0);
+        final IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
+        
+        for (final int slotId : type.getValidSlots())
+            if (inv != null) {
+                
+                final ItemStack stack = inv.getStackInSlot(slotId);
+                
+                if (stack != null)
+                    return stack;
+            }
+        
+        return null;
     }
     
     /**
-     * Checks if a player has a bauble in the ring slot. This will check both slots!
+     * Attempts to get a bauble from the player.
      * 
-     * @param player The player to check for.
-     * @param item The item you are looking for. Only item, meta are accounted for. See
-     *        {@link ItemStackUtils#areStacksSimilar(ItemStack, ItemStack)} for more info.
-     * @return Whether or not the player has a bauble in the desired slot.
+     * @param player The player to get the bauble from.
+     * @param type The slot to get the bauble from.
+     * @return Gets the stack in the slot.
      */
-    @Optional.Method(modid = "Baubles")
-    public static boolean isPlayerWearingRing (EntityPlayer player, ItemStack item) {
+    public static ItemStack getBauble (EntityPlayer player, int type) {
         
-        return isPlayerWearingBauble(player, item, 1) || isPlayerWearingBauble(player, item, 2);
+        final IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
+        
+        if (inv != null) {
+            
+            final ItemStack stack = inv.getStackInSlot(type);
+            
+            if (stack != null)
+                return stack;
+        }
+        
+        return null;
     }
     
     /**
-     * Checks if a player has a bauble in the belt slot.
+     * Checks if a player has a bauble in their inventory.
      * 
-     * @param player The player to check for.
-     * @param item The item you are looking for. Only item, meta are accounted for. See
-     *        {@link ItemStackUtils#areStacksSimilar(ItemStack, ItemStack)} for more info.
-     * @return Whether or not the player has a bauble in the desired slot.
+     * @param player The player to get the bauble from.
+     * @param stack The stack to search for.
+     * @param type The type of bauble being searched for.
+     * @return Whether or not a valid stack was found.
      */
     @Optional.Method(modid = "Baubles")
-    public static boolean isPlayerWearingBelt (EntityPlayer player, ItemStack item) {
+    public static boolean hasBauble (EntityPlayer player, ItemStack stack, BaubleType type) {
         
-        return isPlayerWearingBauble(player, item, 3);
-    }
-    
-    /**
-     * Checks if a player has a bauble in the amulet slot.
-     * 
-     * @param player The player to check for.
-     * @param item The item you are looking for. Only item, meta are accounted for. See
-     *        {@link ItemStackUtils#areStacksSimilar(ItemStack, ItemStack)} for more info.
-     * @param slot The slot to check within. 0 is amulet, 1 and 2 are rings. 3 is belt.
-     * @return Whether or not the player has a bauble in the desired slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static boolean isPlayerWearingBauble (EntityPlayer player, ItemStack item, int slot) {
+        final IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
         
-        return ItemStackUtils.areStacksSimilar(getBauble(player, slot), item);
-    }
-    
-    /**
-     * Retrieves the amulet from the baubles inventory.
-     * 
-     * @param player The player to get the item from.
-     * @return The stack in the amulet slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getAmulet (EntityPlayer player) {
-        
-        return getBauble(player, 0);
-    }
-    
-    /**
-     * Retrieves a ring from the baubles inventory. Does not care which slot, will try to get
-     * the first slot and then the second.
-     * 
-     * @param player The player to get the item from.
-     * @return The first ring that could be found.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getRing (EntityPlayer player) {
-        
-        final ItemStack ring = getFirstRing(player);
-        return ring != null ? ring : getSecondRing(player);
-    }
-    
-    /**
-     * Retrieves the ring from the first ring slot in the baubles inventory.
-     * 
-     * @param player The player to get the item from.
-     * @return The stack in the first ring slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getFirstRing (EntityPlayer player) {
-        
-        return getBauble(player, 1);
-    }
-    
-    /**
-     * Retrieves the ring from the second ring slot in the baubles inventory.
-     * 
-     * @param player The player to get the item from.
-     * @return The stack in the second ring slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getSecondRing (EntityPlayer player) {
-        
-        return getBauble(player, 2);
-    }
-    
-    /**
-     * Retrieves the belt ItemStack from the baubles inventory.
-     * 
-     * @param player The player to get the item from.
-     * @return The stack in the belt slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getBelt (EntityPlayer player) {
-        
-        return getBauble(player, 3);
-    }
-    
-    /**
-     * Retrieves an ItemStack from the baubles inventory.
-     * 
-     * @param player The player to get the item from.
-     * @param slot The slot to search in. 0 is amulet, 1 and 2 are rings. 3 is belt.
-     * @return The stack in the specified bauble slot.
-     */
-    @Optional.Method(modid = "Baubles")
-    public static ItemStack getBauble (EntityPlayer player, int slot) {
-        
-        final IInventory inv = BaublesApi.getBaubles(player);
-        return inv != null ? inv.getStackInSlot(slot) : null;
+        for (final int slotId : type.getValidSlots())
+            if (inv != null && ItemStackUtils.areStacksSimilar(stack, inv.getStackInSlot(slotId)))
+                return true;
+            
+        return false;
     }
     
     /**
@@ -153,11 +95,11 @@ public class BaublesUtils {
     @Optional.Method(modid = "Baubles")
     public static boolean equipBauble (EntityPlayer player, ItemStack item, int slot) {
         
-        final IInventory inv = BaublesApi.getBaubles(player);
+        final IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
         if (inv != null) {
             final ItemStack existing = inv.getStackInSlot(slot);
             if (existing == null) {
-                inv.setInventorySlotContents(slot, item.copy());
+                inv.setStackInSlot(slot, item.copy());
                 item.stackSize--;
                 return true;
             }
--- src/main/java/net/darkhax/bookshelf/lib/modutils/baubles/ItemBauble.java
+++ src/main/java/net/darkhax/bookshelf/lib/modutils/baubles/ItemBauble.java
@@ -1,43 +1,10 @@
 package net.darkhax.bookshelf.lib.modutils.baubles;
 
 import baubles.api.IBauble;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Optional;
 
-/**
- * This implementation of IBauble provides default implementations of most methods. This is to
- * allow cleaner item code, as only one new method is required, and the rest can be overriden
- * as needed.
- */
 @Optional.Interface(modid = "Baubles", iface = "baubles.api.IBauble")
 public abstract class ItemBauble extends Item implements IBauble {
     
-    @Override
-    public void onWornTick (ItemStack itemstack, EntityLivingBase player) {
-        
-    }
-    
-    @Override
-    public void onEquipped (ItemStack itemstack, EntityLivingBase player) {
-        
-    }
-    
-    @Override
-    public void onUnequipped (ItemStack itemstack, EntityLivingBase player) {
-        
-    }
-    
-    @Override
-    public boolean canEquip (ItemStack itemstack, EntityLivingBase player) {
-        
-        return true;
-    }
-    
-    @Override
-    public boolean canUnequip (ItemStack itemstack, EntityLivingBase player) {
-        
-        return true;
-    }
 }
