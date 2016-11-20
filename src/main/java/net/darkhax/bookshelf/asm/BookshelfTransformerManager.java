4a4234a082501ecf4a9f61aefc3b609d5ecc600e
Tyler Hancock <darklime@live.ca>
2016-11-20 05:32:29 -0700

    Removed ASM in favor of Cover's code.

--- src/main/java/net/darkhax/bookshelf/asm/BookshelfTransformerManager.java
+++ src/main/java/net/darkhax/bookshelf/asm/BookshelfTransformerManager.java
@@ -1,6 +1,5 @@
 package net.darkhax.bookshelf.asm;
 
-import net.darkhax.bookshelf.asm.transformers.TransformItemRenderer;
 import net.darkhax.bookshelf.asm.transformers.TransformerEnchantmentHelper;
 import net.minecraft.launchwrapper.IClassTransformer;
 
@@ -8,9 +7,6 @@ public class BookshelfTransformerManager implements IClassTransformer {
     
     @Override
     public byte[] transform (String name, String transformedName, byte[] classBytes) {
-        
-        if (transformedName.equals("net.minecraft.client.renderer.RenderItem"))
-            return TransformItemRenderer.transform(name, transformedName, classBytes);
         
         if (transformedName.equals("net.minecraft.enchantment.EnchantmentHelper"))
             return TransformerEnchantmentHelper.transform(name, transformedName, classBytes);
