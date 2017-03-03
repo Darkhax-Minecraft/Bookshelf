57cbd4bb2c3486bab50f9e24b31f83fe4723bb8b
Tyler Hancock <darklime@live.ca>
2017-03-03 01:52:46 -0700

    Cleaned up the render factory a little bit.

--- src/main/java/net/darkhax/bookshelf/client/render/item/RenderFactoryItem.java
+++ src/main/java/net/darkhax/bookshelf/client/render/item/RenderFactoryItem.java
@@ -3,7 +3,7 @@ package net.darkhax.bookshelf.client.render.item;
 import java.lang.reflect.Field;
 
 import net.darkhax.bookshelf.entity.FakeEntity;
-import net.darkhax.bookshelf.lib.BookshelfException;
+import net.darkhax.bookshelf.lib.Constants;
 import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -16,35 +16,45 @@ import net.minecraftforge.fml.client.registry.IRenderFactory;
  * reference to the global RenderItem class. For internal use only!
  */
 public class RenderFactoryItem implements IRenderFactory<FakeEntity> {
-    
+
     @Override
     public Render<FakeEntity> createRenderFor (RenderManager manager) {
-        
-        try {
-            
-            for (final Render<? extends Entity> render : manager.entityRenderMap.values())
-                if (render != null) {
-                    for (final Field field : render.getClass().getDeclaredFields())
-                        if (field.getType().equals(RenderItem.class)) {
-                            field.setAccessible(true);
-                            field.set(render, RenderItemWrapper.instance());
-                        }
-                }
-        }
-        
-        catch (final Exception e) {
-            
-            throw new BookshelfException("Unable to reflect an EntityRenderer!", e);
-        }
+
+        for (final Render<? extends Entity> render : manager.entityRenderMap.values())
+            if (render != null) {
+                this.patchRenderer(render);
+            }
         
         return new Render<FakeEntity>(manager) {
-            
+
             @Override
             protected ResourceLocation getEntityTexture (FakeEntity entity) {
-                
+
                 return null;
             }
         };
     }
-    
+
+    /**
+     * Attempts to find any field which holds a RenderItem instance, and replaces it with the
+     * RenderItemWrapper from this mod.
+     *
+     * @param render The render to patch.
+     */
+    private void patchRenderer (Render<? extends Entity> render) {
+
+        try {
+
+            for (final Field field : render.getClass().getDeclaredFields()) {
+                if (field.getType().equals(RenderItem.class)) {
+                    field.setAccessible(true);
+                    field.set(render, RenderItemWrapper.instance());
+                }
+            }
+        }
+        catch (IllegalArgumentException | IllegalAccessException e) {
+
+            Constants.LOG.warn("Could not patch renderer!", e);
+        }
+    }
 }
