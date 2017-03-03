2d76946e15463df63ad1d0db6b95447d956a32e2
Tyler Hancock <darklime@live.ca>
2017-03-03 15:04:03 -0700

    Marked a new major update.

--- src/main/java/net/darkhax/bookshelf/lib/Constants.java
+++ src/main/java/net/darkhax/bookshelf/lib/Constants.java
@@ -12,7 +12,7 @@ public final class Constants {
 
     public static final String MOD_NAME = "Bookshelf";
 
-    public static final String VERSION_NUMBER = "1.4.3.0";
+    public static final String VERSION_NUMBER = "1.4.4.0";
 
     public static final String PROXY_CLIENT = "net.darkhax.bookshelf.client.ProxyClient";
 
@@ -24,4 +24,14 @@ public final class Constants {
     public static final Random RANDOM = new Random();
 
     public static final String NEW_LINE = System.getProperty("line.separator");
+
+    /**
+     * Utility classes, such as this one, are not meant to be instantiated. Java adds an
+     * implicit public constructor to every class which does not define at lease one
+     * explicitly. Hence why this constructor was added.
+     */
+    private Constants () {
+
+        throw new IllegalAccessError("Utility class");
+    }
 }
