1917a997436e14448e3b38e75f88768cf1ccb35a
Tyler Hancock <darklime@live.ca>
2017-03-28 10:46:58 -0600

    Added a few new crafting utilities.

--- src/main/java/net/darkhax/bookshelf/lib/util/CraftingUtils.java
+++ src/main/java/net/darkhax/bookshelf/lib/util/CraftingUtils.java
@@ -2,14 +2,23 @@ package net.darkhax.bookshelf.lib.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 
+import net.darkhax.bookshelf.lib.ModTrackingList;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerWorkbench;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.ModContainer;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
@@ -24,6 +33,72 @@ public final class CraftingUtils {
     private CraftingUtils () {
 
         throw new IllegalAccessError("Utility class");
+    }
+
+    /**
+     * Gets the current recipe in a crafting inventory.
+     *
+     * @param craftInv The crafting inventory.
+     * @param world The current world.
+     * @return The recipe being crafted.
+     */
+    public static IRecipe getRecipeForMatrix (InventoryCrafting craftInv, World world) {
+
+        for (final IRecipe recipe : CraftingManager.getInstance().recipes) {
+
+            if (recipe.matches(craftInv, world))
+                return recipe;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the current crafting recipe if the GUI the player has open is a ContainerWorkbench
+     *
+     * @param player The player to get the recipe for.
+     * @return The recipe in the current gui.
+     */
+    public static IRecipe getCurrentCraftingRecipe (EntityPlayer player) {
+
+        final Container container = player.openContainer;
+
+        if (container instanceof ContainerWorkbench) {
+
+            final ContainerWorkbench crafting = (ContainerWorkbench) container;
+            return getRecipeForMatrix(crafting.craftMatrix, player.getEntityWorld());
+        }
+
+        return null;
+    }
+
+    /**
+     * Attempts to get the owner of an IRecipe. This will be the mod that was active when the
+     * recipe was loaded.
+     *
+     * @param recipe The recipe to search.
+     * @return The owner of the recipe. Can be null.
+     */
+    public static ModContainer getOwner (IRecipe recipe) {
+
+        final List<IRecipe> recipes = CraftingManager.getInstance().recipes;
+
+        if (recipes instanceof ModTrackingList)
+            return ((ModTrackingList<IRecipe>) recipes).getModContainer(recipe);
+
+        return null;
+    }
+
+    /**
+     * Gets a map which links IRecipe objects to mod containers. While not every recipe will be
+     * tracked, most of the modded ones will be.
+     *
+     * @return A map which links IRecipe objects to mod containers.
+     */
+    public static Map<IRecipe, ModContainer> getRecipeOwners () {
+
+        final List<IRecipe> recipes = CraftingManager.getInstance().recipes;
+        return recipes instanceof ModTrackingList ? ((ModTrackingList<IRecipe>) recipes).getTrackedEntries() : Collections.emptyMap();
     }
 
     /**
