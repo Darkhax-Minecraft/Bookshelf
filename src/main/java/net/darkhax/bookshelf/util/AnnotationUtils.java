133cb88de6db491d94ab5993c770cd2604bf7097
Tyler Hancock <darklime@live.ca>
2017-04-07 19:57:19 -0600

    Added a few method to the annotation utils.

--- src/main/java/net/darkhax/bookshelf/util/AnnotationUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/AnnotationUtils.java
@@ -1,11 +1,18 @@
 package net.darkhax.bookshelf.util;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import net.darkhax.bookshelf.lib.Constants;
+import net.minecraft.util.Tuple;
 import net.minecraftforge.fml.common.discovery.ASMDataTable;
+import net.minecraftforge.fml.common.discovery.ASMDataTable.ASMData;
 
 public final class AnnotationUtils {
 
@@ -20,21 +27,91 @@ public final class AnnotationUtils {
     }
 
     /**
+     * Gets the ASMData for all classes annotated with the annotation class.
+     *
+     * @param table The ASMDataTable. This is generated from forge an can be accessed from the
+     *        initialization events.
+     * @param annotation The annotation to search for.
+     * @return A set of ASMData for classes with the passed annotation.
+     */
+    public static <A extends Annotation> Set<ASMData> getData (ASMDataTable table, Class<A> annotation) {
+
+        return table.getAll(annotation.getCanonicalName());
+    }
+
+    /**
+     * Gets all classes annotated with the annotation class.
+     *
+     * @param table The ASMDataTable. This is generated from forge and can be accessed from the
+     *        initialization events.
+     * @param annotation The annotation to search for.
+     * @return A list of all classes with the passed annotation.
+     */
+    public static <A extends Annotation> List<Tuple<Class<?>, A>> getAnnotatedClasses (ASMDataTable table, Class<A> annotation) {
+
+        final List<Tuple<Class<?>, A>> classes = new ArrayList<>();
+
+        for (final ASMData data : getData(table, annotation)) {
+
+            try {
+
+                final Class clazz = Class.forName(data.getClassName());
+
+                if (clazz != null) {
+
+                    classes.add(new Tuple<Class<?>, A>(clazz, (A) clazz.getAnnotation(annotation)));
+                }
+            }
+
+            catch (final ClassNotFoundException e) {
+
+                Constants.LOG.warn("Could not load " + data.getClassName(), e);
+            }
+        }
+
+        return classes;
+    }
+
+    /**
+     * Gets all fields annotated with the annotation class.
+     *
+     * @param collection The list of classes to search through.
+     * @param class1 The annotation to search for.
+     * @return A list of all fields with the passed annotation.
+     */
+    public static <A extends Annotation> List<Field> getAnnotatedFields (Collection<Class<?>> collection, Class<A> class1) {
+
+        final List<Field> fields = new ArrayList<>();
+
+        for (final Class<?> clazz : collection) {
+
+            for (final Field field : clazz.getFields()) {
+
+                if (field.isAnnotationPresent(class1)) {
+                    fields.add(field);
+                }
+            }
+        }
+
+        return fields;
+    }
+
+    /**
      * Finds all classes annotated with the annotation class. These classes are then
      * instantiated, added to a list, and given to you.
      *
-     * @param asmDataTable The ASMDataTable created by Forge. You can get this from most of the
-     *        main mod loading stage events.
+     * @param table The ASMDataTable created by Forge. You can get this from most of the main
+     *        mod loading stage events.
      * @param annotation The class of the annotation you're using to search for.
      * @param instance The class of the thing you're trying to construct. This should be a
      *        shared interface, or parent class.
      * @return A list of all classes annotated with the annotation, as instances.
      */
-    public static <T, A extends Annotation> Map<T, A> getAnnotations (ASMDataTable asmDataTable, Class<A> annotation, Class<T> instance) {
+    public static <T, A extends Annotation> Map<T, A> getAnnotations (ASMDataTable table, Class<A> annotation, Class<T> instance) {
 
         final Map<T, A> map = new HashMap<>();
 
-        for (final ASMDataTable.ASMData asmData : asmDataTable.getAll(annotation.getCanonicalName())) {
+        for (final ASMDataTable.ASMData asmData : getData(table, annotation)) {
 
             try {
 
