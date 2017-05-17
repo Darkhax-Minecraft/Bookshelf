10516c291937fb3a8f4ccc5295c6acf3ae9d7513
Tyler Hancock <darklime@live.ca>
2017-05-17 17:06:31 -0600

    Added color handlers.

---
+++ src/main/java/net/darkhax/bookshelf/data/ColorHandlers.java
@@ -0,0 +1,64 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
+package net.darkhax.bookshelf.data;
+
+import net.darkhax.bookshelf.lib.MCColor;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.client.renderer.color.IItemColor;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * A client side only class, which contains references to reusable color handlers for items and
+ * blocks.
+ */
+@SideOnly(Side.CLIENT)
+public class ColorHandlers {
+
+    /**
+     * A reusable color handler which uses the hash of
+     * {@link net.minecraft.item.ItemStack#getDisplayName()}.
+     */
+    public static IItemColor ITEM_DISPLAY_NAME = (stack, index) -> stack.getDisplayName().hashCode();
+
+    /**
+     * A reusable color handler which uses the hash of
+     * {@link net.minecraft.item.ItemStack#getUnlocalizedName()}.
+     */
+    public static IItemColor ITEM_UNLOCALIZED_NAME = (stack, index) -> stack.getUnlocalizedName().hashCode();
+
+    /**
+     * A reusable color handler which uses the hash of the
+     * {@link net.minecraft.item.ItemStack#toString()}.
+     */
+    public static IItemColor ITEM_TO_STRING = (stack, index) -> stack.toString().hashCode();
+
+    /**
+     * A reusable color handler which uses the hash of the
+     * {@link net.minecraft.item.Item#getRegistryName()}.
+     */
+    public static IItemColor ITEM_IDENTIFIER = (stack, index) -> stack.getItem().getRegistryName().toString().hashCode();
+
+    /**
+     * A reusable color handler which uses the hash of the
+     * {@link net.minecraft.nbt.NBTTagCompound#toString()}.
+     */
+    public static IItemColor ITEM_NBT = (stack, index) -> stack.getTagCompound().toString().hashCode();
+
+    /**
+     * A reusable color handler which uses a MCColor which is read from
+     * {@link net.minecraft.item.ItemStack#getTagCompound()};
+     */
+    public static IItemColor ITEM_MCCOLOR = (stack, index) -> MCColor.isAcceptable(stack) ? new MCColor(stack).getRGB() : MCColor.WHITE.getRGB();
+
+    /**
+     * A reusable color handler which uses a MCColor which is read from
+     * {@link net.minecraft.tileentity.TileEntity#getTileData()}.
+     */
+    public static IBlockColor BLOCK_MCCOLOR = (state, world, pos, index) -> MCColor.isAcceptable(world, pos) ? new MCColor(world, pos).getRGB() : MCColor.WHITE.getRGB();
+}
