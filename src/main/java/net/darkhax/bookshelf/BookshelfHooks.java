d600711c42056261ada751c8d811aaa43942e0b7
Tyler Hancock <darklime@live.ca>
2017-05-17 20:48:33 -0600

    Fixed bookshelf hooks.

--- src/main/java/net/darkhax/bookshelf/BookshelfHooks.java
+++ src/main/java/net/darkhax/bookshelf/BookshelfHooks.java
@@ -1,5 +1,19 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
 package net.darkhax.bookshelf;
+
+import net.darkhax.bookshelf.lib.ModTrackingList;
+import net.minecraft.item.crafting.CraftingManager;
 
 public class BookshelfHooks {
 
+    public static void onPrePreInit () {
+
+        CraftingManager.getInstance().recipes = new ModTrackingList(CraftingManager.getInstance().recipes);
+    }
 }
