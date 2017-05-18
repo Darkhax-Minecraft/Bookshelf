199a751fa23361bc14f024ecc3eea3ff06b47b2c
Tyler Hancock <darklime@live.ca>
2017-05-17 19:39:01 -0600

    Added config.

---
+++ src/main/java/net/darkhax/bookshelf/config/Config.java
@@ -0,0 +1,31 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
+package net.darkhax.bookshelf.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * This annotation should be added to any class with a configurable property. This is used as a
+ * homing beacon to find annotations on the fields in the class you add this to.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Config {
+
+    /**
+     * The name/id for the config. This is used to create the file name for the config, and all
+     * configurable fields in the class will be added to the file with this name. I highly
+     * recommend using the modid for this, and only having one config file per mod.
+     *
+     * @return The name for the config file this is associated with.
+     */
+    String name();
+}
