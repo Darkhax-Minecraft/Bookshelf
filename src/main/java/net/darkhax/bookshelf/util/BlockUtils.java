eca27450128dced2c62d076db079b5b8ab936766
Tyler Hancock <darklime@live.ca>
2017-08-28 20:16:24 -0600

    Added some methods for dealing with fluids and block states.

--- src/main/java/net/darkhax/bookshelf/util/BlockUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/BlockUtils.java
@@ -10,9 +10,17 @@ package net.darkhax.bookshelf.util;
 import javax.annotation.Nonnull;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.BlockOre;
+import net.minecraft.block.BlockStaticLiquid;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -65,5 +73,93 @@ public final class BlockUtils {
     public static boolean isFluid (Block block) {
 
         return block == Blocks.LAVA || block == Blocks.WATER || block instanceof IFluidBlock;
+    }
+
+    /**
+     * Checks if the fluid level is full.
+     *
+     * @param world The world.
+     * @param pos The position.
+     * @return Whether or not the fluid is full.
+     */
+    public static boolean isFluidFull (World world, BlockPos pos) {
+
+        return isFluidFull(getActualState(world, pos));
+    }
+
+    /**
+     * Checks if the fluid level is full.
+     *
+     * @param state The block state.
+     * @return Whether or not the fluid is full.
+     */
+    public static boolean isFluidFull (IBlockState state) {
+
+        return getFluidLevel(state) == 0;
+    }
+
+    /**
+     * Get the fluid level.
+     *
+     * @param world The world.
+     * @param pos The position.
+     * @return The level of the fluid. 0 is full.
+     */
+    public static int getFluidLevel (World world, BlockPos pos) {
+
+        return getFluidLevel(getActualState(world, pos));
+    }
+
+    /**
+     * Get the fluid level.
+     *
+     * @param state The block state.
+     * @return The level of the fluid. 0 is full.
+     */
+    public static int getFluidLevel (IBlockState state) {
+
+        return state.getValue(BlockLiquid.LEVEL);
+    }
+
+    /**
+     * Attempts to get a fluid stack from a block pos.
+     *
+     * @param world The world.
+     * @param pos The position.
+     * @return The fluid stack.
+     */
+    public static FluidStack getFluid (World world, BlockPos pos) {
+
+        final IBlockState state = getActualState(world, pos);
+        final Block block = state.getBlock();
+
+        if (block instanceof IFluidBlock && ((IFluidBlock) block).canDrain(world, pos)) {
+
+            return ((IFluidBlock) block).drain(world, pos, true);
+        }
+
+        else if (block instanceof BlockStaticLiquid && isFluidFull(state)) {
+
+            final Fluid fluid = block == Blocks.WATER ? FluidRegistry.WATER : block == Blocks.LAVA ? FluidRegistry.LAVA : null;
+
+            if (fluid != null) {
+
+                return new FluidStack(fluid, 1000);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the actual state of the block.
+     *
+     * @param world The world.
+     * @param pos The position.
+     * @return The actual block state.
+     */
+    public static IBlockState getActualState (World world, BlockPos pos) {
+
+        return world.getBlockState(pos).getActualState(world, pos);
     }
 }
