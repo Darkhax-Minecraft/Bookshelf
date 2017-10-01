e374e7bb635086330cf62464c2d1be9373c89234
Tyler Hancock <darklime@live.ca>
2017-09-30 23:19:18 -0600

    Added a basic biome utility for geting biomes of multiple types.

---
+++ src/main/java/net/darkhax/bookshelf/util/BiomeUtils.java
@@ -0,0 +1,23 @@
+package net.darkhax.bookshelf.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
+
+public class BiomeUtils {
+
+    public static Set<Biome> getBiomesForTypes (Type... types) {
+
+        final Set<Biome> biomes = new HashSet<>();
+
+        for (final Type type : types) {
+
+            biomes.addAll(BiomeDictionary.getBiomes(type));
+        }
+
+        return biomes;
+    }
+}
