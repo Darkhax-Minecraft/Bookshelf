a76a4db986372aee9ce22f48fc8b0258cadc90b0
Tyler Hancock <darklime@live.ca>
2017-11-24 20:27:02 -0700

    Added some missing javadocs for IColorfulItem and ICustomMesh

--- src/main/java/net/darkhax/bookshelf/item/IColorfulItem.java
+++ src/main/java/net/darkhax/bookshelf/item/IColorfulItem.java
@@ -1,11 +1,28 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
 package net.darkhax.bookshelf.item;
 
 import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/**
+ * This interface is used by the bookshelf registry system to automatically detect and register
+ * item color handlers appropriately, without using proxies or any other complicated spread out
+ * code.
+ */
 public interface IColorfulItem {
 
+    /**
+     * Gets the color handler for the item.
+     *
+     * @return The color handler for the item.
+     */
     @SideOnly(Side.CLIENT)
     public IItemColor getColorHandler ();
 }
--- src/main/java/net/darkhax/bookshelf/item/ICustomMesh.java
+++ src/main/java/net/darkhax/bookshelf/item/ICustomMesh.java
@@ -1,9 +1,20 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
 package net.darkhax.bookshelf.item;
 
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+/**
+ * This interface is used by bookshelf's registry to automatically detect an item that has
+ * custom mesh definitions. This can be used to register your own model stuff.
+ */
 public interface ICustomMesh {
 
     /**
