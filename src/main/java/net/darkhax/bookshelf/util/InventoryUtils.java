cdb93a7a96a59294855dee517e85b285f6c899cb
Tyler Hancock <darklime@live.ca>
2019-01-30 13:11:59 -0700

    Added some methods to help access and check container listeners.

--- src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
@@ -7,6 +7,10 @@
  */
 package net.darkhax.bookshelf.util;
 
+import java.util.List;
+
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IContainerListener;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -14,17 +18,17 @@ import net.minecraft.util.NonNullList;
 import net.minecraftforge.common.util.Constants;
 
 public final class InventoryUtils {
-    
+
     /**
      * Utility classes, such as this one, are not meant to be instantiated. Java adds an
      * implicit public constructor to every class which does not define at lease one
      * explicitly. Hence why this constructor was added.
      */
     private InventoryUtils() {
-        
+
         throw new IllegalAccessError("Utility class");
     }
-    
+
     /**
      * Write an inventory as an NBTTagCompound. See {@link #readInventory(NBTTagCompound)} for
      * how this can be read.
@@ -33,26 +37,26 @@ public final class InventoryUtils {
      * @return An NBTTagCompound which holds all of the info for the inventory.
      */
     public static NBTTagCompound writeInventory (NonNullList<ItemStack> inventory) {
-        
+
         final NBTTagList items = new NBTTagList();
-        
+
         for (int slot = 0; slot < inventory.size(); slot++) {
-            
+
             if (!inventory.get(slot).isEmpty()) {
-                
+
                 final NBTTagCompound itemTag = new NBTTagCompound();
                 itemTag.setInteger("Slot", slot);
                 inventory.get(slot).writeToNBT(itemTag);
                 items.appendTag(itemTag);
             }
         }
-        
+
         final NBTTagCompound tag = new NBTTagCompound();
         tag.setTag("Items", items);
         tag.setInteger("Size", inventory.size());
         return tag;
     }
-    
+
     /**
      * Reads an inventory from an NBTTagCompound. Check {@link #writeInventory(NonNullList)}
      * for the type of tag supported. Size of the inventory is set based on the item tag list
@@ -62,18 +66,42 @@ public final class InventoryUtils {
      * @return A NonNullList which contains the read inventory contents.
      */
     public static NonNullList<ItemStack> readInventory (NBTTagCompound tag) {
-        
+
         final NonNullList<ItemStack> stacks = NonNullList.withSize(tag.getInteger("Size"), ItemStack.EMPTY);
-        
+
         final NBTTagList tagList = tag.getTagList("Items", Constants.NBT.TAG_COMPOUND);
-        
+
         for (int i = 0; i < tagList.tagCount(); i++) {
-            
+
             final NBTTagCompound itemTag = tagList.getCompoundTagAt(i);
             final int slot = itemTag.getInteger("Slot");
             stacks.set(slot, new ItemStack(itemTag));
         }
-        
+
         return stacks;
+    }
+
+    /**
+     * Wrapper for {@link Container#listeners} field. This wrapper exists due to access
+     * transformers.
+     *
+     * @param container The container to access.
+     * @return A list of the container listeners.
+     */
+    public static List<IContainerListener> getListeners (Container container) {
+
+        return container.listeners;
+    }
+
+    /**
+     * Checks if a listener is on a container.
+     *
+     * @param container The container to check.
+     * @param listener The listener to look for.
+     * @return Whether or not the container has the listener.
+     */
+    public static boolean hasListener (Container container, IContainerListener listener) {
+
+        return getListeners(container).contains(listener);
     }
 }
