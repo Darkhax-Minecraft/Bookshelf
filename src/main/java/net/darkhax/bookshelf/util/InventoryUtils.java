208836c66f4deb53d47278a6be41c434c3ca260d
Tyler Hancock <darklime@live.ca>
2019-04-25 13:11:04 -0600

    Added basic class for inventory utils.

---
+++ src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
@@ -0,0 +1,33 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
+package net.darkhax.bookshelf.util;
+
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.EmptyHandler;
+
+public class InventoryUtils {
+
+    public static IItemHandler getInventory(World world, BlockPos pos, EnumFacing side) {
+        
+        final TileEntity tileEntity = world.getTileEntity(pos);
+        
+        if (tileEntity != null) {
+            
+            final LazyOptional<IItemHandler> inventoryCap = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side);
+            return inventoryCap.orElse(EmptyHandler.INSTANCE);
+        }
+        
+        return null;
+    }
+}
