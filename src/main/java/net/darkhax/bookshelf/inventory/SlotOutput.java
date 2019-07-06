f8377685b37d5f2fbe8c5b150a1796093c49a2c4
Tyler Hancock <darklime@live.ca>
2019-07-06 13:41:50 -0600

    Added some inventory and slot classes that can use lambdas as inputs for common use cases.

---
+++ src/main/java/net/darkhax/bookshelf/inventory/SlotOutput.java
@@ -0,0 +1,58 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
+package net.darkhax.bookshelf.inventory;
+
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
+
+/**
+ * This slot provides a slot with optional validator for inputs, and a listener for when the
+ * item is taken from the slot.
+ */
+public class SlotOutput extends Slot {
+    
+    /**
+     * A predicate that validates what items can be placed in this slot.
+     */
+    private final Predicate<ItemStack> inputValidator;
+    
+    /**
+     * A listener that is called when an item is removed from the slot.
+     */
+    private final BiConsumer<PlayerEntity, ItemStack> takeListener;
+    
+    public SlotOutput(IInventory inventory, int index, int xPosition, int yPosition, BiConsumer<PlayerEntity, ItemStack> takeListener) {
+        
+        this(inventory, index, xPosition, yPosition, stack -> false, takeListener);
+    }
+    
+    public SlotOutput(IInventory inventory, int index, int xPosition, int yPosition, Predicate<ItemStack> inputValidator, BiConsumer<PlayerEntity, ItemStack> takeListener) {
+        
+        super(inventory, index, xPosition, yPosition);
+        this.inputValidator = inputValidator;
+        this.takeListener = takeListener;
+    }
+    
+    @Override
+    public boolean isItemValid (ItemStack stack) {
+        
+        return this.inputValidator.test(stack);
+    }
+    
+    @Override
+    public ItemStack onTake (PlayerEntity player, ItemStack stack) {
+        
+        this.takeListener.accept(player, stack);
+        return super.onTake(player, stack);
+    }
+}
