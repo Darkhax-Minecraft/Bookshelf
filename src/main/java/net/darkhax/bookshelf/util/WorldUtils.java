9b4475207a4dc3b7d5c01bbceb1dd5e48287ef87
Tyler Hancock <darklime@live.ca>
2019-07-05 21:37:51 -0600

    Removed chunk code related to changing biomes. This will be changed later.

--- src/main/java/net/darkhax/bookshelf/util/WorldUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/WorldUtils.java
@@ -8,11 +8,9 @@
 package net.darkhax.bookshelf.util;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-import net.darkhax.bookshelf.Bookshelf;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.IWorldPosCallable;
@@ -20,7 +18,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 
 public final class WorldUtils {
@@ -46,70 +43,6 @@ public final class WorldUtils {
     public static boolean areSameChunk (BlockPos first, BlockPos second) {
         
         return new ChunkPos(first).equals(new ChunkPos(second));
-    }
-    
-    /**
-     * Attempts to set the biome of an entire chunk. Please note that this will also cause
-     * conecting chunks to do an update, and will cause the targeted chunk to recieve a render
-     * update.
-     *
-     * @param world The world to set the biome in.
-     * @param pos The block position to target. This will target the chunk the psotion is in.
-     * @param biome The biome to set the chunk to.
-     */
-    public static void setBiomes (World world, BlockPos pos, Biome biome) {
-        
-        try {
-            
-            final Chunk chunk = world.getChunkAt(pos);
-            final Biome[] biomes = chunk.getBiomes();
-            Arrays.fill(biomes, biome);
-            chunk.markDirty();
-            
-            updateNearbyChunks(world, chunk, true, true);
-        }
-        
-        catch (final Exception e) {
-            
-            Bookshelf.LOG.catching(e);
-            Bookshelf.LOG.warn("Unable to set biome for Pos: {}, Biome: {}", pos.toString(), biome.getRegistryName());
-        }
-    }
-    
-    /**
-     * Marks a chunk for an update. This will set it dirty, and potentially do a render update.
-     *
-     * @param world The world to update chunks in.
-     * @param chunk The chunk to update.
-     * @param render Whether or not you want a render update.
-     */
-    public static void markChunkForUpdate (World world, Chunk chunk, boolean render) {
-        
-        chunk.markDirty();
-        
-        // TODO Re-implement rendering behaviour
-        // if (render) {
-        //
-        // final BlockPos initial = chunk.getPos().getBlock(1, 1, 1);
-        // world.markBlockRangeForRenderUpdate(initial, initial);
-        // }
-    }
-    
-    /**
-     * Updates all chunks near the provided chunk. This is a 3x3 area centered on the chunk.
-     *
-     * @param world The world to update chunks in.
-     * @param chunk The chunk to update.
-     * @param includeSelf Whether or not the base chunk should be updated.
-     * @param render Whether or not you want a render update.
-     */
-    public static void updateNearbyChunks (World world, Chunk chunk, boolean includeSelf, boolean render) {
-        
-        for (final Chunk other : getNearbyChunks(world, chunk)) {
-            if (other != chunk || includeSelf) {
-                markChunkForUpdate(world, other, render);
-            }
-        }
     }
     
     /**
