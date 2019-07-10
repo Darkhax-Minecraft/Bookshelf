7af421a1c5b9fa14fcb4e59e6826e67564f00d51
Tyler Hancock <darklime@live.ca>
2019-07-10 14:16:31 -0600

    Created the basis for Bookshelf's own commands.

--- src/main/java/net/darkhax/bookshelf/Bookshelf.java
+++ src/main/java/net/darkhax/bookshelf/Bookshelf.java
@@ -12,7 +12,11 @@ import java.util.Random;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.darkhax.bookshelf.registry.RegistryHelper;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Bookshelf.MOD_ID)
 public class Bookshelf {
@@ -28,4 +32,13 @@ public class Bookshelf {
     public static final String MOD_NAME = "Bookshelf";
     
     public static final Logger LOG = LogManager.getLogger(MOD_NAME);
+    
+    private final RegistryHelper registry = new RegistryHelper(MOD_ID, LOG, null);
+    
+    public Bookshelf() {
+        
+        this.registry.initialize(FMLJavaModLoadingContext.get().getModEventBus());
+        this.registry.registerBlock(new Block(net.minecraft.block.Block.Properties.create(Material.SAND)), "test_block");
+        new BookshelfCommands(this.registry);
+    }
 }
---
+++ src/main/java/net/darkhax/bookshelf/BookshelfCommands.java
@@ -0,0 +1,69 @@
+package net.darkhax.bookshelf;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+
+import net.darkhax.bookshelf.command.ArgumentTypeMod;
+import net.darkhax.bookshelf.registry.RegistryHelper;
+import net.darkhax.bookshelf.util.CommandUtils;
+import net.darkhax.bookshelf.util.WorldUtils;
+import net.minecraft.block.Block;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.Commands;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class BookshelfCommands {
+    
+    private static final String LOOT_TABLE_BLOCK_DROP_SELF = "{\"type\":\"minecraft:block\",\"pools\":[{\"rolls\":1,\"entries\":[{\"type\":\"minecraft:item\",\"name\":\"%output%\"}],\"conditions\":[{\"condition\":\"minecraft:survives_explosion\"}]}]}";
+    private static final File OUTPUT_DIR = new File("dump/bookshelf");
+    
+    public BookshelfCommands(RegistryHelper registry) {
+        
+        final LiteralArgumentBuilder<CommandSource> root = Commands.literal("bookshelf");
+        root.then(CommandUtils.createModCommand("validateLootTables", 2, this::validateLootTables));
+        
+        registry.registerCommand(root);
+    }
+    
+    private int validateLootTables (CommandContext<CommandSource> context, boolean wildcard) {
+        
+        final String modId = wildcard ? "every mod" : ArgumentTypeMod.getMod(context, "mod");
+        int foundIssues = 0;
+        
+        for (final Block block : ForgeRegistries.BLOCKS) {
+            
+            if ((wildcard || block.getRegistryName().getNamespace().equals(modId)) && !WorldUtils.doesLootTableExist(context.getSource().getWorld(), block.getLootTable())) {
+                
+                final ResourceLocation id = block.getRegistryName();
+                final File outputFile = new File(OUTPUT_DIR, "data/" + id.getNamespace() + "/loot_tables/blocks/" + id.getPath() + ".json");
+                
+                try {
+                    
+                    FileUtils.forceMkdir(outputFile.getParentFile());
+                    
+                    try (FileWriter writer = new FileWriter(outputFile)) {
+                        
+                        writer.write(LOOT_TABLE_BLOCK_DROP_SELF.replace("%output%", id.toString()));
+                    }
+                }
+                
+                catch (final IOException e) {
+                    
+                    Bookshelf.LOG.error("Failed to write dummy block loot table.", e);
+                }
+                foundIssues++;
+            }
+        }
+        
+        context.getSource().sendFeedback(new TranslationTextComponent("commands.bookshelf.loot_tables", foundIssues, modId), false);
+        return 0;
+    }
+}
