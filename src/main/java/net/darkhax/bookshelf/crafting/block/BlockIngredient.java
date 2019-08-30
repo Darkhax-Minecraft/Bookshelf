2f930129930da4d8b4a8a80adb614d42f9543b02
Tyler Hancock <darklime@live.ca>
2019-08-30 16:26:34 -0600

    Fix getValidBlocks not loading valid blocks.

--- src/main/java/net/darkhax/bookshelf/crafting/block/BlockIngredient.java
+++ src/main/java/net/darkhax/bookshelf/crafting/block/BlockIngredient.java
@@ -150,6 +150,7 @@ public class BlockIngredient implements Predicate<Block> {
      */
     public List<Block> getValidBlocks () {
         
+        this.determineMatchingBlocks();
         return this.validBlocks;
     }
     
