7f28c5b97119de42a12904caf221bfcdb073372c
Tyler Hancock <darklime@live.ca>
2019-11-23 15:22:46 -0700

    Spruced up the inventory utils.

--- src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
@@ -18,6 +18,15 @@ import net.minecraftforge.items.wrapper.EmptyHandler;
 
 public class InventoryUtils {
     
+    /**
+     * Gets an inventory from a position within the world. If no tile exists or the tile does
+     * not have the inventory capability the empty inventory handler will be returned.
+     * 
+     * @param world The world instance.
+     * @param pos The position of the expected tile entity.
+     * @param side The side to access the inventory from.
+     * @return The inventory handler. Will be empty if none was found.
+     */
     public static IItemHandler getInventory (World world, BlockPos pos, Direction side) {
         
         final TileEntity tileEntity = world.getTileEntity(pos);
@@ -28,6 +37,21 @@ public class InventoryUtils {
             return inventoryCap.orElse(EmptyHandler.INSTANCE);
         }
         
-        return null;
+        return EmptyHandler.INSTANCE;
+    }
+    
+    /**
+     * Checks if a position has an inventory. The empty inventory handler will be considered
+     * invalid.
+     * 
+     * @param world The world instance.
+     * @param pos The position of the expected tile entity.
+     * @param side The side to access the inventory from.
+     * @return Whether or not the inventory exists.
+     */
+    public static boolean hasInventory (World world, BlockPos pos, Direction side) {
+        
+        final IItemHandler handler = getInventory(world, pos, side);
+        return handler != null && handler != EmptyHandler.INSTANCE;
     }
 }
