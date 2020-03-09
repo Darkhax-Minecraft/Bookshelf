b4a0de2b76e29dd53957a7c47d24bc2adda18b97
Tyler Hancock <darklime@live.ca>
2020-03-08 20:54:04 -0600

    Added support for detecting blocks like composters which don't use tile entities but still have inventories.

--- src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/InventoryUtils.java
@@ -7,6 +7,9 @@
  */
 package net.darkhax.bookshelf.util;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.inventory.ISidedInventoryProvider;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
@@ -15,6 +18,7 @@ import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.EmptyHandler;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
 
 public class InventoryUtils {
     
@@ -35,6 +39,24 @@ public class InventoryUtils {
             
             final LazyOptional<IItemHandler> inventoryCap = tileEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, side);
             return inventoryCap.orElse(EmptyHandler.INSTANCE);
+        }
+        
+        else {
+            
+            // Some blocks like composters are not tile entities so their inv can not be
+            // accessed through the normal capability system.
+            final BlockState state = world.getBlockState(pos);
+            
+            if (state.getBlock() instanceof ISidedInventoryProvider) {
+                
+                final ISidedInventoryProvider inventoryProvider = (ISidedInventoryProvider) state.getBlock();
+                final ISidedInventory inventory = inventoryProvider.createInventory(state, world, pos);
+                
+                if (inventory != null) {
+                    
+                    return new SidedInvWrapper(inventory, side);
+                }
+            }
         }
         
         return EmptyHandler.INSTANCE;
