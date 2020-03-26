9943fbc844135a6afbcd3a3b5d109cedb9c1d2f2
Tyler Hancock <darklime@live.ca>
2020-03-25 21:48:14 -0600

    Added modifier enchantment base.

---
+++ src/main/java/net/darkhax/bookshelf/enchantment/CurseEnchantmentModifier.java
@@ -0,0 +1,54 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
+package net.darkhax.bookshelf.enchantment;
+
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.inventory.EquipmentSlotType;
+
+public class CurseEnchantmentModifier extends EnchantmentAttribute {
+    
+    public CurseEnchantmentModifier(EnchantmentType type, EquipmentSlotType... slots) {
+        
+        this(Rarity.VERY_RARE, type, slots);
+    }
+    
+    public CurseEnchantmentModifier(Rarity rarity, EnchantmentType type, EquipmentSlotType... slots) {
+        
+        super(rarity, type, slots);
+    }
+    
+    @Override
+    public int getMinEnchantability (int level) {
+        
+        return 25;
+    }
+    
+    @Override
+    public int getMaxEnchantability (int level) {
+        
+        return 50;
+    }
+    
+    @Override
+    public int getMaxLevel () {
+        
+        return 1;
+    }
+    
+    @Override
+    public boolean isTreasureEnchantment () {
+        
+        return true;
+    }
+    
+    @Override
+    public boolean isCurse () {
+        
+        return true;
+    }
+}
