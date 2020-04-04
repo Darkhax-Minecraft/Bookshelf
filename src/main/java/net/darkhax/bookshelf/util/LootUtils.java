3968ac025105cc7bcca6640884d0d5a002456aeb
Tyler Hancock <darklime@live.ca>
2020-04-03 23:26:17 -0600

    Added docs to loot utils and and fixed a bug with conditions being read as loot entries.

--- src/main/java/net/darkhax/bookshelf/util/LootUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/LootUtils.java
@@ -1,3 +1,10 @@
+/**
+ * This class was created by <Darkhax>. It is distributed as part of Bookshelf. You can find
+ * the original source here: https://github.com/Darkhax-Minecraft/Bookshelf
+ *
+ * Bookshelf is Open Source and distributed under the GNU Lesser General Public License version
+ * 2.1.
+ */
 package net.darkhax.bookshelf.util;
 
 import java.util.List;
@@ -9,27 +16,51 @@ import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.storage.loot.LootEntry;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.conditions.ILootCondition;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
-public class LootUtils {
+public final class LootUtils {
     
+    /**
+     * Maps a list of loot pools using their names.
+     * 
+     * @param pools The pools to map.
+     * @return An immutable map of loot pools.
+     */
     public static Map<String, LootPool> mapPools (List<LootPool> pools) {
         
-        return ImmutableMap.copyOf(pools.stream().collect(Collectors.toMap(p -> p.getName(), p -> p)));
-        
+        return ImmutableMap.copyOf(pools.stream().collect(Collectors.toMap(LootPool::getName, p -> p)));
     }
     
+    /**
+     * Gets all loot pools within a table.
+     * 
+     * @param table The table to pull from.
+     * @return The list of pools within a table.
+     */
     public static List<LootPool> getPools (LootTable table) {
         
         return ObfuscationReflectionHelper.getPrivateValue(LootTable.class, table, "field_186466_c");
     }
     
+    /**
+     * Gets all loot entries from a loot pool.
+     * 
+     * @param pool The loot pool to pull from.
+     * @return The list of entries within the pool.
+     */
     public static List<LootEntry> getEntries (LootPool pool) {
         
         return ObfuscationReflectionHelper.getPrivateValue(LootPool.class, pool, "field_186453_a");
     }
     
-    public static List<LootEntry> getConditions (LootPool pool) {
+    /**
+     * Gets all loot conditions from a loot pool.
+     * 
+     * @param pool The loot pool to pull from.
+     * @return The list of loot conditions.
+     */
+    public static List<ILootCondition> getConditions (LootPool pool) {
         
         return ObfuscationReflectionHelper.getPrivateValue(LootPool.class, pool, "field_186454_b");
     }
