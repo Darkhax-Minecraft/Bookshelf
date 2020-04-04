41a6475802faffc4690f25d0745c2f5bd68aeb1e
Tyler Hancock <darklime@live.ca>
2020-04-04 01:29:46 -0600

    Added a check item predicate which can be used for weapons as well.

---
+++ src/main/java/net/darkhax/bookshelf/loot/condition/CheckItem.java
@@ -0,0 +1,59 @@
+package net.darkhax.bookshelf.loot.condition;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+
+import net.darkhax.bookshelf.Bookshelf;
+import net.darkhax.bookshelf.util.LootUtils;
+import net.minecraft.advancements.criterion.ItemPredicate;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.conditions.ILootCondition;
+
+public class CheckItem implements ILootCondition {
+    
+    /**
+     * The serializer for this function.
+     */
+    public static final Serializer SERIALIZER = new Serializer();
+    
+    /**
+     * The predicate applied to the first item.
+     */
+    private final ItemPredicate predicate;
+    
+    public CheckItem(ItemPredicate predicate) {
+        
+        this.predicate = predicate;
+    }
+    
+    @Override
+    public boolean test (LootContext ctx) {
+        
+        final ItemStack stack = LootUtils.getItemContext(ctx);
+        return stack != null ? this.predicate.test(stack) : false;
+    }
+    
+    static class Serializer extends ILootCondition.AbstractSerializer<CheckItem> {
+        
+        Serializer() {
+            
+            super(new ResourceLocation(Bookshelf.MOD_ID, "check_item"), CheckItem.class);
+        }
+        
+        @Override
+        public void serialize (JsonObject json, CheckItem value, JsonSerializationContext context) {
+            
+            json.add("predicate", value.predicate.serialize());
+        }
+        
+        @Override
+        public CheckItem deserialize (JsonObject json, JsonDeserializationContext context) {
+            
+            final ItemPredicate predicate = ItemPredicate.deserialize(json.get("predicate"));
+            return new CheckItem(predicate);
+        }
+    }
+}
