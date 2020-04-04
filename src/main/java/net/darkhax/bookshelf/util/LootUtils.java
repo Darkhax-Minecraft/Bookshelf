fdddcdfcdb2eb653b7c2d89c70a7d62709342c09
Tyler Hancock <darklime@live.ca>
2020-04-04 01:29:00 -0600

    Added a util to read loot item from context.

--- src/main/java/net/darkhax/bookshelf/util/LootUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/LootUtils.java
@@ -11,9 +11,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.annotation.Nullable;
+
 import com.google.common.collect.ImmutableMap;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.storage.loot.LootContext;
 import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootParameters;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.conditions.ILootCondition;
@@ -63,5 +70,37 @@ public final class LootUtils {
     public static List<ILootCondition> getConditions (LootPool pool) {
         
         return ObfuscationReflectionHelper.getPrivateValue(LootPool.class, pool, "field_186454_b");
+    }
+    
+    /**
+     * Gets an ItemStack used with a given loot context. It first checks for the tool context.
+     * If no tool is found it will check for the killer's held item. If there is no killer it
+     * will check the direct killer.
+     * 
+     * @param ctx The loot context to read from.
+     * @return The stack that was found for the given context. This may be null.
+     */
+    @Nullable
+    public static ItemStack getItemContext (LootContext ctx) {
+        
+        ItemStack stack = ctx.get(LootParameters.TOOL);
+        
+        // In some cases like killing an entity the tool is null rather than ItemStack.EMPTY.
+        if (stack == null) {
+            
+            Entity killer = ctx.get(LootParameters.KILLER_ENTITY);
+            
+            if (killer == null) {
+                
+                killer = ctx.get(LootParameters.DIRECT_KILLER_ENTITY);
+            }
+            
+            if (killer instanceof LivingEntity) {
+                
+                stack = ((LivingEntity) killer).getHeldItemMainhand();
+            }
+        }
+        
+        return stack;
     }
 }
