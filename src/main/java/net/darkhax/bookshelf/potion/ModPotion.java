a4ccc5a02674c978ef76e57adff40acfe09ba9e8
Tyler Hancock <darklime@live.ca>
2020-09-23 12:14:26 -0600

    Added Potion type with nicer translation keys for mods.

---
+++ src/main/java/net/darkhax/bookshelf/potion/ModPotion.java
@@ -0,0 +1,20 @@
+package net.darkhax.bookshelf.potion;
+
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Potion;
+
+public class ModPotion extends Potion {
+    
+    public ModPotion(EffectInstance... effects) {
+        
+        super(null, effects);
+    }
+    
+    @Override
+    public String getNamePrefixed (String prefix) {
+        
+        // Vanilla just appends the path, but for mod compatibility including the namespace is
+        // better.
+        return prefix + this.getRegistryName().getNamespace() + "." + this.getRegistryName().getPath();
+    }
+}
