198554c3bf52688417f0542436104a2f53e09ca4
Tyler Hancock <darklime@live.ca>
2020-11-10 22:42:01 -0700

    Added util to check if a command argument exists in a given context.

--- src/main/java/net/darkhax/bookshelf/util/CommandUtils.java
+++ src/main/java/net/darkhax/bookshelf/util/CommandUtils.java
@@ -7,14 +7,18 @@
  */
 package net.darkhax.bookshelf.util;
 
+import java.util.Map;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
 
 import net.darkhax.bookshelf.command.ArgumentTypeMod;
 import net.darkhax.bookshelf.command.BooleanCommand;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.command.arguments.EntityArgument;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class CommandUtils {
     
@@ -56,5 +60,18 @@ public class CommandUtils {
     public static LiteralArgumentBuilder<CommandSource> createModCommand (String key, int permissions, BooleanCommand command) {
         
         return Commands.literal(key).requires(sender -> sender.hasPermissionLevel(permissions)).executes(ctx -> command.apply(ctx, true)).then(Commands.argument("mod", ArgumentTypeMod.INSTACE).executes(ctx -> command.apply(ctx, false)));
+    }
+    
+    /**
+     * Checks if the command context contains a given argument.
+     * 
+     * @param context The command context.
+     * @param arg The argument being searched for.
+     * @return Whether or not the argument was present.
+     */
+    public static boolean hasArgument (CommandContext<?> context, String arg) {
+        
+        final Map<String, ?> arguments = ObfuscationReflectionHelper.getPrivateValue(CommandContext.class, context, "arguments");
+        return arguments.containsKey(arg);
     }
 }
