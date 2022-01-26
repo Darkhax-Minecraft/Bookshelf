f332e8bb685a18b73cf5ce5ec384598f61fe991e
Tyler Hancock <darklime@live.ca>
2022-01-26 00:48:27 -0700

    Add helper for working with ItemStacks.

---
+++ Common/src/main/java/net/darkhax/bookshelf/api/util/ItemStackHelper.java
@@ -0,0 +1,54 @@
+package net.darkhax.bookshelf.api.util;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+
+/**
+ * This class contains static helper methods for working with ItemStacks.
+ */
+public final class ItemStackHelper {
+
+    /**
+     * Calculates the attack damage for an ItemStack when used to attack an unspecified mob. This will include bonus
+     * damage sources such as modifiers and enchantments.
+     *
+     * @param stack The ItemStack to calculate the damage value of.
+     * @return The attack damage for an ItemStack with bonus damage modifiers applied.
+     */
+    public static double getAttackDamage(ItemStack stack) {
+
+        return getAttackDamage(stack, MobType.UNDEFINED);
+    }
+
+    /**
+     * Calculates the attack damage for an ItemStack when used to attack a specific entity. This will include bonus
+     * damage sources such as attribute modifiers and enchantments.
+     *
+     * @param stack  The ItemStack to calculate the damage value of.
+     * @param target The target entity being attacked.
+     * @return The attack damage for an ItemStack with bonus damage modifiers applied.
+     */
+    public static double getAttackDamage(ItemStack stack, Entity target) {
+
+        return getAttackDamage(stack, (target instanceof LivingEntity living) ? living.getMobType() : MobType.UNDEFINED);
+    }
+
+    /**
+     * Calculates the attack damage for an ItemStack after accounting for bonus damage modifiers such as attribute
+     * modifiers and enchantments.
+     *
+     * @param stack      The ItemStack to calculate the damage value of.
+     * @param targetType The type of mob being attacked. Used for some enchantment calculations.
+     * @return The attack damage for an ItemStack with bonus damage modifiers applied.
+     */
+    public static double getAttackDamage(ItemStack stack, MobType targetType) {
+
+        final double damage = AttributeHelper.getAttackDamage(stack);
+        final double bonusEnchantmentDamage = EnchantmentHelper.getDamageBonus(stack, targetType);
+
+        return damage + bonusEnchantmentDamage;
+    }
+}
