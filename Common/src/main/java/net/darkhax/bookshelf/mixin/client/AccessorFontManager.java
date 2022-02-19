e0bf19370bb033b9f98409714d59fc687d006c24
Tyler Hancock <darklime@live.ca>
2022-02-19 05:02:53 -0700

    Added several accessors for font and text data.

---
+++ Common/src/main/java/net/darkhax/bookshelf/mixin/client/AccessorFontManager.java
@@ -0,0 +1,16 @@
+package net.darkhax.bookshelf.mixin.client;
+
+import net.minecraft.client.gui.font.FontManager;
+import net.minecraft.client.gui.font.FontSet;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(FontManager.class)
+public interface AccessorFontManager {
+
+    @Accessor("fontSets")
+    Map<ResourceLocation, FontSet> bookshelf$getFonts();
+}
