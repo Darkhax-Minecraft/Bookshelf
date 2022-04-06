76dfbda81d2590de43a449aa19fca1d237dfb4f2
Tyler Hancock <darklime@live.ca>
2022-04-06 17:53:57 -0600

    Add some groundwork for creating new recipe variants.

---
+++ Common/src/main/java/net/darkhax/bookshelf/api/data/recipes/RecipeBaseData.java
@@ -0,0 +1,52 @@
+package net.darkhax.bookshelf.api.data.recipes;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+/**
+ * A partial implementation of {@link net.minecraft.world.item.crafting.Recipe} that is intended for recipes that are
+ * not crafted through an inventory or traditional crafting system. This implementation provides default behaviours to
+ * ignore the vanilla crafting system.
+ * <p>
+ * This approach is often preferred over defining your own reload listener as it allows the data to be synced to the
+ * client and maintains a load order that is beneficial for implementing support for mods like JEI and CraftTweaker.
+ *
+ * @param <C> The type of inventory used to craft the recipe. This can usually be ignored.
+ */
+public abstract class RecipeBaseData<C extends Container> extends RecipeBase<C> {
+
+    public RecipeBaseData(ResourceLocation id) {
+
+        super(id);
+    }
+
+    @Override
+    public boolean matches(C inventory, Level level) {
+
+        // This is not used by default.
+        return false;
+    }
+
+    @Override
+    public ItemStack assemble(C c) {
+
+        // This is not used by default.
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int width, int height) {
+
+        // This is not used by default.
+        return false;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+
+        // This is not used by default.
+        return ItemStack.EMPTY;
+    }
+}
