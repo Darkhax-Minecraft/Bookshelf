375c10e31e26c74d09e5297186131a8341931dc0
Tyler Hancock <darklime@live.ca>
2022-04-07 14:20:56 -0600

    Add new Random constant.

--- Common/src/main/java/net/darkhax/bookshelf/Constants.java
+++ Common/src/main/java/net/darkhax/bookshelf/Constants.java
@@ -3,9 +3,12 @@ package net.darkhax.bookshelf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+
 public class Constants {
 
     public static final String MOD_ID = "bookshelf";
     public static final String MOD_NAME = "Bookshelf";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+    public static final Random RANDOM = new Random();
 }
