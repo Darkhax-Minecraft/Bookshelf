33843e1491a55721dea2b45a817dede1d9ba78f2
Tyler Hancock <darklime@live.ca>
2022-06-04 01:56:18 -0600

    Expose WeightedRandomList total weight and entries.

---
+++ Common/src/main/java/net/darkhax/bookshelf/mixin/util/random/AccessorWeightedRandomList.java
@@ -0,0 +1,16 @@
+package net.darkhax.bookshelf.mixin.util.random;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.util.random.WeightedRandomList;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(WeightedRandomList.class)
+public interface AccessorWeightedRandomList<E> {
+
+    @Accessor("totalWeight")
+    int bookshelf$getTotalWeight();
+
+    @Accessor("items")
+    ImmutableList<E> bookshelf$getEntries();
+}
