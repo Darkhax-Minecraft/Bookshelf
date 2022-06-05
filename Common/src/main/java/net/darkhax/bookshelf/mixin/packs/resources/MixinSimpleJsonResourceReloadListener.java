0fc07935fec6ff998480ad36be27ccfbcd9bd593
Tyler Hancock <darklime@live.ca>
2022-06-05 01:07:24 -0600

    Fix load condition compilations.

--- Common/src/main/java/net/darkhax/bookshelf/mixin/packs/resources/MixinSimpleJsonResourceReloadListener.java
+++ Common/src/main/java/net/darkhax/bookshelf/mixin/packs/resources/MixinSimpleJsonResourceReloadListener.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.darkhax.bookshelf.api.data.conditions.ILoadCondition;
 import net.darkhax.bookshelf.api.data.conditions.ILoadConditionSerializer;
+import net.darkhax.bookshelf.api.data.conditions.LoadConditions;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
@@ -21,6 +22,6 @@ public class MixinSimpleJsonResourceReloadListener {
     @Inject(method = "prepare(Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)Ljava/util/Map;", at = @At("RETURN"))
     private void prepare(ResourceManager manager, ProfilerFiller profiler, CallbackInfoReturnable<Map<ResourceLocation, JsonElement>> cbi) {
 
-        cbi.getReturnValue().entrySet().removeIf(entry -> entry.getValue() instanceof JsonObject obj && !ILoadConditionSerializer.canLoad(obj));
+        cbi.getReturnValue().entrySet().removeIf(entry -> entry.getValue() instanceof JsonObject obj && !LoadConditions.canLoad(obj));
     }
 }
