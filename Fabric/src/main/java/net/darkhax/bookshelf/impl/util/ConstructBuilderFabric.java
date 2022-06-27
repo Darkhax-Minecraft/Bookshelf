ff93f907e855f8bd23e0dfaad0605bb98edae0ec
Tyler Hancock <darklime@live.ca>
2022-06-27 02:41:05 -0600

    Add loader neutral way to construct new menu types.

--- Fabric/src/main/java/net/darkhax/bookshelf/impl/util/ConstructBuilderFabric.java
+++ Fabric/src/main/java/net/darkhax/bookshelf/impl/util/ConstructBuilderFabric.java
@@ -3,12 +3,18 @@ package net.darkhax.bookshelf.impl.util;
 import net.darkhax.bookshelf.api.item.ICreativeTabBuilder;
 import net.darkhax.bookshelf.api.util.IConstructHelper;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.apache.commons.lang3.function.TriFunction;
 
 import java.util.Arrays;
 import java.util.function.BiFunction;
@@ -26,5 +32,11 @@ public class ConstructBuilderFabric implements IConstructHelper {
     public <T extends BlockEntity> Supplier<BlockEntityType<T>> blockEntityType(BiFunction<BlockPos, BlockState, T> factory, Block... validBlocks) {
 
         return () -> FabricBlockEntityTypeBuilder.create(factory::apply, validBlocks).build();
+    }
+
+    @Override
+    public <T extends AbstractContainerMenu> MenuType<T> menuType(TriFunction<Integer, Inventory, FriendlyByteBuf, T> constructor) {
+
+        return new ExtendedScreenHandlerType<>(constructor::apply);
     }
 }
