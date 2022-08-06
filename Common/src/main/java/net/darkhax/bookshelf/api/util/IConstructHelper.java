2567b17b3598c82ff4f993c55c605adbbac8762f
Tyler Hancock <darklime@live.ca>
2022-08-06 04:17:02 -0600

    Add support for constructing menu types.

--- Common/src/main/java/net/darkhax/bookshelf/api/util/IConstructHelper.java
+++ Common/src/main/java/net/darkhax/bookshelf/api/util/IConstructHelper.java
@@ -2,10 +2,15 @@ package net.darkhax.bookshelf.api.util;
 
 import net.darkhax.bookshelf.api.item.ICreativeTabBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import org.apache.commons.lang3.function.TriFunction;
 
 import java.util.Collection;
 import java.util.function.BiFunction;
@@ -21,4 +26,6 @@ public interface IConstructHelper {
     }
 
     <T extends BlockEntity> Supplier<BlockEntityType<T>> blockEntityType(BiFunction<BlockPos, BlockState, T> factory, Block... blocks);
+
+    <T extends AbstractContainerMenu> MenuType<T> menuType(TriFunction<Integer, Inventory, FriendlyByteBuf, T> constructor);
 }
--- Forge/src/main/java/net/darkhax/bookshelf/impl/util/ConstructHelperForge.java
+++ Forge/src/main/java/net/darkhax/bookshelf/impl/util/ConstructHelperForge.java
@@ -4,11 +4,17 @@ import net.darkhax.bookshelf.api.item.ICreativeTabBuilder;
 import net.darkhax.bookshelf.api.util.IConstructHelper;
 import net.darkhax.bookshelf.impl.item.CreativeTabBuilderForge;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.extensions.IForgeMenuType;
+import org.apache.commons.lang3.function.TriFunction;
 
 import java.util.Arrays;
 import java.util.function.BiFunction;
@@ -26,5 +32,11 @@ public class ConstructHelperForge implements IConstructHelper {
     public <T extends BlockEntity> Supplier<BlockEntityType<T>> blockEntityType(BiFunction<BlockPos, BlockState, T> factory, Block... validBlocks) {
 
         return () -> BlockEntityType.Builder.of(factory::apply, validBlocks).build(null);
+    }
+
+    @Override
+    public <T extends AbstractContainerMenu> MenuType<T> menuType(TriFunction<Integer, Inventory, FriendlyByteBuf, T> constructor) {
+
+        return IForgeMenuType.create(constructor::apply);
     }
 }
