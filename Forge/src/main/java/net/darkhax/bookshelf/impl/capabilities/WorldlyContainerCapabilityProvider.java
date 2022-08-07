199083002335f24b38c4a43c1bb9caed488786b0
Tyler Hancock <darklime@live.ca>
2022-08-07 06:57:56 -0600

    Improve sided inventory lookup.

--- Forge/src/main/java/net/darkhax/bookshelf/impl/capabilities/WorldlyContainerCapabilityProvider.java
+++ Forge/src/main/java/net/darkhax/bookshelf/impl/capabilities/WorldlyContainerCapabilityProvider.java
@@ -1,40 +1,33 @@
 package net.darkhax.bookshelf.impl.capabilities;
 
-import net.darkhax.bookshelf.api.function.CachedSupplier;
 import net.minecraft.core.Direction;
 import net.minecraft.world.WorldlyContainer;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.InvWrapper;
 import net.minecraftforge.items.wrapper.SidedInvWrapper;
 
 import javax.annotation.Nullable;
-import java.util.EnumMap;
 
 public class WorldlyContainerCapabilityProvider implements ICapabilityProvider {
 
-    private final CachedSupplier<EnumMap<Direction, LazyOptional<IItemHandler>>> sidedCaps;
+    private final WorldlyContainer container;
 
     public WorldlyContainerCapabilityProvider(WorldlyContainer container) {
 
-        sidedCaps = CachedSupplier.cache(() -> {
-
-            final EnumMap<Direction, LazyOptional<IItemHandler>> caps = new EnumMap<>(Direction.class);
-
-            for (Direction side : Direction.values()) {
-
-                caps.put(side, container.getSlotsForFace(side).length > 0 ? LazyOptional.of(() -> new SidedInvWrapper(container, side)) : LazyOptional.empty());
-            }
-
-            return caps;
-        });
+        this.container = container;
     }
 
     @Override
     public <R> LazyOptional<R> getCapability(Capability<R> requested, @Nullable Direction side) {
 
-        return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(requested, sidedCaps.get().get(side).cast());
+        if (requested == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+
+            return LazyOptional.of(() -> (side != null) ? new SidedInvWrapper(container, side) : new InvWrapper(container)).cast();
+        }
+
+        return LazyOptional.empty();
     }
 }
