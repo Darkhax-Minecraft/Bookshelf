40da2f5ca4ff0bb3f897f512146a2ef26f9f07da
Tyler Hancock <darklime@live.ca>
2022-08-07 06:26:24 -0600

    Add support for serializing nullable values.

--- Common/src/main/java/net/darkhax/bookshelf/impl/gametest/TestSerialization.java
+++ Common/src/main/java/net/darkhax/bookshelf/impl/gametest/TestSerialization.java
@@ -14,10 +14,8 @@ import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.util.random.WeightedEntry;
-import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -368,6 +366,81 @@ public class TestSerialization<T> implements ITestable {
 
         helper.succeed();
     }
+
+    @GameTest
+    public void testJsonNullable(GameTestHelper helper) {
+
+        final JsonElement nullJson = this.serializer.toJSONNullable(null);
+
+        if (nullJson != null) {
+
+            helper.fail("Expected JSON to be null.");
+        }
+
+        final T nullValue = this.serializer.fromJSONNullable(nullJson);
+
+        if (nullValue != null) {
+
+            helper.fail("Expected value to be null.");
+        }
+
+        final JsonElement json = this.serializer.toJSONNullable(this.singleton);
+
+        if (json == null) {
+
+            helper.fail("Expected JSON to not be null.");
+        }
+
+        final T value = this.serializer.fromJSONNullable(json);
+
+        if (value == null) {
+
+            helper.fail("Expected value to not be null.");
+        }
+
+        if (!this.equality.test(this.singleton, value)) {
+
+            helper.fail("Value written and read do not match.");
+        }
+
+        else {
+
+            helper.succeed();
+        }
+    }
+
+    @GameTest
+    public void testBytebufNullable(GameTestHelper helper) {
+
+        final FriendlyByteBuf nullBuffer = new FriendlyByteBuf(Unpooled.buffer());
+        this.serializer.toByteBufNullable(nullBuffer, null);
+        final T nullValue = this.serializer.fromByteBufNullable(nullBuffer);
+
+        if (nullValue != null) {
+
+            helper.fail("Expected value to be null.");
+        }
+
+        final FriendlyByteBuf buffer = new FriendlyByteBuf(Unpooled.buffer());
+        this.serializer.toByteBufNullable(buffer, this.singleton);
+        final T value = this.serializer.fromByteBufNullable(buffer);
+
+        if (value == null) {
+
+            helper.fail("Expected value to not be null.");
+        }
+
+        if (!this.equality.test(this.singleton, value)) {
+
+            helper.fail("Value written and read do not match.");
+        }
+
+        else {
+
+            helper.succeed();
+        }
+    }
+
 
     private Set<T> createSet() {
 
