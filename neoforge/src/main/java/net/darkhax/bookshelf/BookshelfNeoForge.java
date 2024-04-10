ac07bdf6f766093ef2a79acdac5eeb6edf5c580b
Tyler Hancock <darklime@live.ca>
2024-04-10 04:56:13 -0600

    Fix inventory wrapping on NeoForge.

--- neoforge/src/main/java/net/darkhax/bookshelf/BookshelfNeoForge.java
+++ neoforge/src/main/java/net/darkhax/bookshelf/BookshelfNeoForge.java
@@ -3,15 +3,12 @@ package net.darkhax.bookshelf;
 import net.darkhax.bookshelf.api.block.entity.WorldlyInventoryBlockEntity;
 import net.darkhax.bookshelf.api.util.IConstructHelper;
 import net.darkhax.bookshelf.impl.BookshelfCommon;
-import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.capabilities.BlockCapability;
+import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
-import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.items.wrapper.InvWrapper;
 import net.neoforged.neoforge.items.wrapper.SidedInvWrapper;
 
@@ -24,9 +21,6 @@ public class BookshelfNeoForge {
         modBus.addListener(this::attachBlockCapabilities);
     }
 
-    private static final ResourceLocation WORLDLY_CONTAINER_WRAPPER = Constants.id("worldly_container_wrapper");
-    private static final BlockCapability<IItemHandler, Direction> WRAPPER_CAP = BlockCapability.createSided(WORLDLY_CONTAINER_WRAPPER, IItemHandler.class);
-
     private void attachBlockCapabilities(RegisterCapabilitiesEvent event) {
 
         for (BlockEntityType<?> type : BuiltInRegistries.BLOCK_ENTITY_TYPE) {
@@ -37,7 +31,7 @@ public class BookshelfNeoForge {
 
                 if (WorldlyInventoryBlockEntity.class.isAssignableFrom(blockEntityClass)) {
 
-                    event.registerBlockEntity(WRAPPER_CAP, type, (blockEntity, side) -> {
+                    event.registerBlockEntity(Capabilities.ItemHandler.BLOCK, type, (blockEntity, side) -> {
 
                         if (blockEntity instanceof WorldlyInventoryBlockEntity<?> worldly) {
 
