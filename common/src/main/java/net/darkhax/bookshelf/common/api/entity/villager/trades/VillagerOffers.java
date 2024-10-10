8354c892b96f98bc770c592687864b0d936f1051
Tyler Hancock <darklime@live.ca>
2024-10-10 01:46:59 -0600

    Fix VillagerOffers not accepting offer suppliers.

--- common/src/main/java/net/darkhax/bookshelf/common/api/entity/villager/trades/VillagerOffers.java
+++ common/src/main/java/net/darkhax/bookshelf/common/api/entity/villager/trades/VillagerOffers.java
@@ -6,15 +6,16 @@ import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.trading.MerchantOffer;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Supplier;
+
 /**
  * A simple villager trade entry that selects a random offer from an equally weighted array of offers.
  *
  * @param offers An equally weighted array of offers.
  */
-public record VillagerOffers(MerchantOffer... offers) implements VillagerTrades.ItemListing {
-
+public record VillagerOffers(Supplier<MerchantOffer>... offers) implements VillagerTrades.ItemListing {
     @Override
     public MerchantOffer getOffer(@NotNull Entity entity, @NotNull RandomSource randomSource) {
-        return offers[randomSource.nextInt(offers.length)];
+        return offers[randomSource.nextInt(offers.length)].get();
     }
 }
