20f60cf51f02c1e83907b8660c64eb2a4f6fd028
Tyler Hancock <darklime@live.ca>
2024-10-14 21:47:19 -0600

    Update villager trade documentation.

--- common/src/main/java/net/darkhax/bookshelf/common/api/registry/register/RegisterVillagerTrades.java
+++ common/src/main/java/net/darkhax/bookshelf/common/api/registry/register/RegisterVillagerTrades.java
@@ -18,22 +18,54 @@ public class RegisterVillagerTrades {
     private final List<VillagerTrades.ItemListing> rareTrades = new ArrayList<>();
     private final List<VillagerTrades.ItemListing> commonTrades = new ArrayList<>();
 
+    /**
+     * Adds a new trade listing to a specific profession that may become available when the specified profession tier is
+     * reached.
+     *
+     * @param profession The profession to register the trade to.
+     * @param tier       The tier at which this trade can become available.
+     * @param trade      The trade listing to add.
+     */
     public void addTrade(VillagerProfession profession, int tier, VillagerTrades.ItemListing trade) {
         villagerTrades.computeIfAbsent(profession, p -> ArrayListMultimap.create()).put(tier, trade);
     }
 
+    /**
+     * Adds a new trade listing to a specific profession that may become available when the specified profession tier is
+     * reached.
+     *
+     * @param profession The profession to register the trade to.
+     * @param tier       The tier at which this trade can become available.
+     * @param trade      The trade listing to add.
+     */
     public void addTrade(VillagerProfession profession, MerchantTier tier, VillagerTrades.ItemListing trade) {
         this.addTrade(profession, tier.ordinal() + 1, trade);
     }
 
+    /**
+     * Adds a trade to the wandering villager trade pool.
+     *
+     * @param trade  The trade to add.
+     * @param isRare If the trade belongs in the rare or common pool.
+     */
     public void addWanderingTrade(VillagerTrades.ItemListing trade, boolean isRare) {
         (isRare ? rareTrades : commonTrades).add(trade);
     }
 
+    /**
+     * Adds a trade to the wandering traders rare trades pool.
+     *
+     * @param trade The trade to add.
+     */
     public void addRareWanderingTrade(VillagerTrades.ItemListing trade) {
         this.addWanderingTrade(trade, true);
     }
 
+    /**
+     * Adds a trade to the wandering traders common trade pool.
+     *
+     * @param trade The trade to add.
+     */
     public void addCommonWanderingTrade(VillagerTrades.ItemListing trade) {
         this.addWanderingTrade(trade, false);
     }
@@ -45,7 +77,7 @@ public class RegisterVillagerTrades {
     public List<VillagerTrades.ItemListing> getRareWanderingTrades() {
         return Collections.unmodifiableList(this.rareTrades);
     }
-
+    
     public List<VillagerTrades.ItemListing> getCommonWanderingTrades() {
         return Collections.unmodifiableList(this.commonTrades);
     }
