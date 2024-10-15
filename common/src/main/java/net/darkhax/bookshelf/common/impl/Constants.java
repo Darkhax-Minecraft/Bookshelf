449cd2bfccefe8610c2466caa071afb184518ddb
Tyler Hancock <darklime@live.ca>
2024-10-14 21:36:14 -0600

    Intern custom ResourceLocations to reduce memory usage.

--- common/src/main/java/net/darkhax/bookshelf/common/impl/Constants.java
+++ common/src/main/java/net/darkhax/bookshelf/common/impl/Constants.java
@@ -6,14 +6,18 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
 
     public static final String MOD_ID = "bookshelf";
     public static final String MOD_NAME = "Bookshelf";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
     public static final Gson GSON_PRETTY = new GsonBuilder().setPrettyPrinting().create();
+    private static final Map<String, ResourceLocation> ID_CACHE = new HashMap<>();
 
     public static ResourceLocation id(String path) {
-        return ResourceLocation.tryBuild(MOD_ID, path);
+        return ID_CACHE.computeIfAbsent(path, p -> ResourceLocation.tryBuild(MOD_ID, p));
     }
 }
