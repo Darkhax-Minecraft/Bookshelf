77f4fb5d6877a52680248ab61764fba7ac2d7c5e
Tyler Hancock <darklime@live.ca>
2025-01-17 21:31:22 -0700

    Add a StreamCodec for enum types.

---
+++ common/src/main/java/net/darkhax/bookshelf/common/api/data/codecs/EnumStreamCodec.java
@@ -0,0 +1,25 @@
+package net.darkhax.bookshelf.common.api.data.codecs;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import org.jetbrains.annotations.NotNull;
+
+public class EnumStreamCodec<T extends Enum<T>> implements StreamCodec<FriendlyByteBuf, T> {
+
+	private final Class<T> enumClass;
+
+	public EnumStreamCodec(Class<T> clazz) {
+		this.enumClass = clazz;
+	}
+
+	@NotNull
+	@Override
+	public T decode(FriendlyByteBuf buf) {
+		return buf.readEnum(enumClass);
+	}
+
+	@Override
+	public void encode(FriendlyByteBuf buf, @NotNull T toWrite) {
+		buf.writeEnum(toWrite);
+	}
+}
