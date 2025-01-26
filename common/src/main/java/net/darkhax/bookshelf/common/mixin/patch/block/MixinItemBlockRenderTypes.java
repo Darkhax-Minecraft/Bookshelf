e4b968ba08ab8bced8865283489b1fd650d8e8ae
Tyler Hancock <darklime@live.ca>
2025-01-26 13:28:37 -0700

    Fix block render type map being set to immutable when it should not be.

--- common/src/main/java/net/darkhax/bookshelf/common/mixin/patch/block/MixinItemBlockRenderTypes.java
+++ common/src/main/java/net/darkhax/bookshelf/common/mixin/patch/block/MixinItemBlockRenderTypes.java
@@ -9,11 +9,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
-@Mixin(value = ItemBlockRenderTypes.class, priority = 900)
+@Mixin(value = ItemBlockRenderTypes.class)
 public class MixinItemBlockRenderTypes {
 
     @Shadow
@@ -22,8 +20,6 @@ public class MixinItemBlockRenderTypes {
     private static Map<Block, RenderType> TYPE_BY_BLOCK;
 
     static {
-        TYPE_BY_BLOCK = new HashMap<>(TYPE_BY_BLOCK);
         Services.CONTENT_PROVIDERS.get().forEach(provider -> provider.bindRenderLayers(TYPE_BY_BLOCK::put));
-        TYPE_BY_BLOCK = Collections.unmodifiableMap(TYPE_BY_BLOCK);
     }
 }
