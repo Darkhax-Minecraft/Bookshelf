155117f1b19aadd3cdd7206dbf84646d32588a2f
Tyler Hancock <darklime@live.ca>
2025-01-30 14:05:35 -0700

    Remove unused imports.

--- common/src/main/java/net/darkhax/bookshelf/common/api/function/ReloadableCache.java
+++ common/src/main/java/net/darkhax/bookshelf/common/api/function/ReloadableCache.java
@@ -12,12 +12,10 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
-import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.ref.WeakReference;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
