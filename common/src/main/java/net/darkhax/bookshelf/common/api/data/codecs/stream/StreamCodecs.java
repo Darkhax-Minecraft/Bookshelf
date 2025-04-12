46ae55271b3b42c81d5d7df56a5663b85d1af07c
Tyler Hancock <darklime@live.ca>
2025-04-11 22:29:06 -0600

    Create new StreamCodec for ingredient that is never empty.

--- common/src/main/java/net/darkhax/bookshelf/common/api/data/codecs/stream/StreamCodecs.java
+++ common/src/main/java/net/darkhax/bookshelf/common/api/data/codecs/stream/StreamCodecs.java
@@ -1,10 +1,12 @@
 package net.darkhax.bookshelf.common.api.data.codecs.stream;
 
 import io.netty.buffer.ByteBuf;
+import net.darkhax.bookshelf.common.impl.data.ingredient.FalseIngredient;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,13 @@ public class StreamCodecs {
 
     public static final StreamCodec<RegistryFriendlyByteBuf, String> STRING = StreamCodec.of(FriendlyByteBuf::writeUtf, FriendlyByteBuf::readUtf);
     public static final StreamCodec<RegistryFriendlyByteBuf, List<ItemStack>> ITEM_STACK_LIST = list(ItemStack.STREAM_CODEC);
+    public static final StreamCodec<RegistryFriendlyByteBuf, Ingredient> INGREDIENT_NON_EMPTY = StreamCodec.of(
+            Ingredient.CONTENTS_STREAM_CODEC,
+            buf -> {
+                final Ingredient ingredient = Ingredient.CONTENTS_STREAM_CODEC.decode(buf);
+                return ingredient.isEmpty() ? FalseIngredient.INSTANCE.get() : ingredient;
+            }
+    );
 
     public static <B extends ByteBuf, V> StreamCodec<B, List<V>> list(StreamCodec<B, V> baseCodec) {
         return StreamCodec.of(
