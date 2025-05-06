2a2bd30ffab9f7a37e765aa3fe22f4ec4ff1add0
Tyler Hancock <darklime@live.ca>
2025-05-05 22:47:45 -0600

    Create an annotation to mark internal use only APIs.

---
+++ common/src/main/java/net/darkhax/bookshelf/common/api/annotation/InternalUse.java
@@ -0,0 +1,8 @@
+package net.darkhax.bookshelf.common.api.annotation;
+
+/**
+ * A visual indicator for members that may be visible for technical reasons or convenience but are not intended for
+ * general use. For example, if a method you did not create is decorated with this annotation you should not invoke it.
+ */
+public @interface InternalUse {
+}
