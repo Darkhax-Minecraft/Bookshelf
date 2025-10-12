73c2ceb3bbf9d19b4f3917a13aba9098e0fe50c6
Tyler Hancock <darklime@live.ca>
2025-10-11 19:02:43 -0600

    Add a helper to detect services without loading or initializing them.

--- common/src/main/java/net/darkhax/bookshelf/common/api/service/Services.java
+++ common/src/main/java/net/darkhax/bookshelf/common/api/service/Services.java
@@ -7,6 +7,14 @@ import net.darkhax.bookshelf.common.api.util.IGameplayHelper;
 import net.darkhax.bookshelf.common.api.util.IPlatformHelper;
 import net.darkhax.bookshelf.common.impl.Constants;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.stream.Collectors;
@@ -28,5 +36,30 @@ public class Services {
         final List<T> entries = ServiceLoader.load(clazz).stream().map(ServiceLoader.Provider::get).toList();
         Constants.LOG.debug("Loaded {} entries for {}. {}", entries.size(), clazz, entries.stream().map(entry -> entry.getClass().getCanonicalName()).collect(Collectors.joining()));
         return entries;
+    }
+
+    /**
+     * Finds implementations of a service without initializing or classloading them.
+     *
+     * @param name The fully qualified name of the service.
+     * @return A list of all implementations that were found.
+     * @throws IOException Sometimes stuff can't be read.
+     */
+    public static List<String> findServices(String name) throws IOException {
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final List<String> matches = new ArrayList<>();
+        final Enumeration<URL> candidates = classLoader.getResources("META-INF/services/" + name);
+        while (candidates.hasMoreElements()) {
+            try (InputStream input = candidates.nextElement().openStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (!line.startsWith("#") && !line.isEmpty()) {
+                        matches.add(line);
+                    }
+                }
+            }
+        }
+        return matches;
     }
 }
