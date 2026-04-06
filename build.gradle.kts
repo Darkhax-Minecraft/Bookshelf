510f17e476d28ff859a6ea767f1d33c36086c40a
Tyler Hancock <darklime@live.ca>
2026-04-06 15:43:11 -0600

    Disable max error reporting.
    $exclude_changelog$

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,3 @@
 plugins {
     id("net.darkhax.mmc") version "26.1.1.6"
 }
-
-allprojects {
-    gradle.projectsEvaluated {
-        tasks.withType<JavaCompile>().configureEach {
-            options.compilerArgs.addAll(listOf("-Xmaxerrs", "9999"))
-        }
-    }
-}
